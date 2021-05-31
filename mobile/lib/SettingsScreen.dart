b3ce23f090cb1c92cbb488d4c0839a5ebe98ac0a
ConnorMayston <cmayston0911@gmail.com>
2021-05-31 19:25:46 +0200

    Added imports and created stateful widget for SettingsScreen.dart

--- mobile/lib/SettingsScreen.dart
+++ mobile/lib/SettingsScreen.dart
@@ -1,6 +1,18 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class SettingsScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  File? _image;
+  final picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Container();

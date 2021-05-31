da49e0d8368a2226703edcae32807dac367e2aaf
Josh-Reddy <josh.reddy01@gmail.com>
2021-05-31 19:42:58 +0200

    Added a Password field for RegistrationScreen.dart class, Joshua Reddy

--- mobile/lib/RegistrationScreen.dart
+++ mobile/lib/RegistrationScreen.dart
@@ -39,6 +39,18 @@ class RegistrationScreen extends StatelessWidget {
               ],
             ),
           ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 20.0),
+            child: Column(
+              children: [
+                Text(
+                  "Password: ",
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                TextField()
+              ],
+            ),
+          ),
 
         ],
       ),

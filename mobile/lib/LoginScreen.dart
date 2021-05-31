fc8c277ed0494ca0b16f8f8322e24db8f9167f1b
Josh-Reddy <josh.reddy01@gmail.com>
2021-05-31 18:42:38 +0200

    Added an app bar for the LoginScreen.dart class, Joshua Reddy

--- mobile/lib/LoginScreen.dart
+++ mobile/lib/LoginScreen.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back),
+          )
+        ],
+        title: Text("Atbash Login"),
+        backgroundColor: Colors.green,
+      ),
+
+
+    );
   }
 }

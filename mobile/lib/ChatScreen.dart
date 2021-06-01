1496f0486cd72f99ae704bd63e0e1cee59fd488b
Blackbird0911 <liamm0912@gmail.com>
2021-06-01 10:21:35 +0200

    Created basic outline for ChatScreen class.

--- mobile/lib/ChatScreen.dart
+++ mobile/lib/ChatScreen.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 
+import 'MainScreen.dart';
+
 class ChatScreen extends StatelessWidget {
+  final List<String> messages = [
+    "Hello!",
+    "This is a test message.",
+    "Goodbye =D"
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: ChatScreenAppBar(),
+        body: Card(
+          child: ListView(
+            children: _buildMessages(),
+          ),
+        )
+    );
   }
+
+  List<Widget> _buildMessages() {
+    List<Widget> messages = [];
+    //TODO: populate messages
+    return messages;
+  }
+}
+
+class ChatScreenAppBar extends AppBar {
+  ChatScreenAppBar() : super(
+    title: Row(
+      children: [
+        EmptyProfileIcon(Colors.white),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Joshua",
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }

4e94645289271b868415ea4414d17a3baa977cfe
DYLERN <dylan@squarepeg.co.za>
2021-06-02 16:27:11 +0200

    Added get userChats to UserModel

--- mobile/lib/model/UserModel.dart
+++ mobile/lib/model/UserModel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:mobile/domain/Chat.dart';
+import 'package:mobile/domain/Contact.dart';
 import 'package:mobile/domain/User.dart';
 
 class UserModel extends ChangeNotifier {
@@ -8,6 +10,12 @@ class UserModel extends ChangeNotifier {
   void logInUser(String phoneNumber, String displayName) {
     _loggedInUser = User(phoneNumber, displayName, "Hi! I'm using Atbash");
   }
+
+  List<Contact> get userChats =>
+      _loggedInUser?.contacts
+          .where((Contact contact) => contact.chat != null)
+          .toList() ??
+      [];
 
   void changeUserDisplayName(String displayName) {
     _loggedInUser?.displayName = displayName;

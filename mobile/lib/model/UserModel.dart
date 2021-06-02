11d24961a430e040f9a4f72f022f05f40446e982
DYLERN <dylan@squarepeg.co.za>
2021-06-02 14:57:34 +0200

    Added mock login to UserModel

--- mobile/lib/model/UserModel.dart
+++ mobile/lib/model/UserModel.dart
@@ -4,6 +4,11 @@ import 'package:mobile/domain/User.dart';
 class UserModel extends ChangeNotifier {
   User? _loggedInUser;
 
+  // TODO this is a mock function
+  void logInUser(String phoneNumber, String displayName) {
+    _loggedInUser = User(phoneNumber, displayName, "Hi! I'm using Atbash");
+  }
+
   void changeUserDisplayName(String displayName) {
     _loggedInUser?.displayName = displayName;
     notifyListeners();

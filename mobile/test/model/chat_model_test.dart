4add6f7b11fb9ebdf26b23fbf2271643cab1ffbf
Blackbird0911 <liamm0912@gmail.com>
2021-06-03 13:11:11 +0200

    Created and implemented chat model unit testing.

---
+++ mobile/test/model/chat_model_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/domain/Chat.dart';
+import 'package:mobile/model/ChatModel.dart';
+
+void main() {
+  test("New message should be sent and recieved", () {
+    final Chat chat = Chat();
+
+    final chatModel = ChatModel(chat);
+
+    chatModel.addMessage("", "any", "message");
+    expect(2, chatModel.messages.length);
+  });
+
+  test("Empty string sent, no messages should be sent or received", () {
+    final Chat chat = Chat();
+
+    final chatModel = ChatModel(chat);
+
+    chatModel.addMessage("", "any", "");
+    expect(0, chatModel.messages.length);
+  });
+
+  test("String with only spaces sent, no messages should be sent or received",
+      () {
+    final Chat chat = Chat();
+
+    final chatModel = ChatModel(chat);
+
+    chatModel.addMessage("", "any", "");
+    expect(0, chatModel.messages.length);
+  });
+
+  test(
+      "String with only spaces and newlines sent, no messages should be sent or received",
+      () {
+    final Chat chat = Chat();
+
+    final chatModel = ChatModel(chat);
+
+    chatModel.addMessage("", "any", " \n \n ");
+    expect(0, chatModel.messages.length);
+  });
+
+  test("String with only newlines sent, no messages should be sent or received",
+      () {
+    final Chat chat = Chat();
+
+    final chatModel = ChatModel(chat);
+
+    chatModel.addMessage("", "any", " \n \n ");
+    expect(0, chatModel.messages.length);
+  });
+}

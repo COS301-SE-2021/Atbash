3917d9d884144c3f2c3be7e93400d737622b14be
ConnorMayston <cmayston0911@gmail.com>
2021-06-14 16:26:57 +0200

    Finished UserService.kt verifyLogin

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
@@ -1,6 +1,7 @@
 package za.ac.up.cs.atbash.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import za.ac.up.cs.atbash.domain.User
 import za.ac.up.cs.atbash.repository.UserRepository
@@ -12,8 +13,17 @@ class UserService(@Autowired private val userRepository: UserRepository) {
         return null
     }
 
-    fun verifyLogin(number: String, password: String): Boolean {
-        return false
+    fun verifyLogin(number: String, password: String): String? {
+        val user = userRepository.findByNumber(number)
+        return if(user != null){
+            if(BCryptPasswordEncoder().matches(password, user.password)){
+                user.apiKey
+            }else{
+                null
+            }
+        }else{
+            null
+        }
     }
 
     fun getUserByNumber(number: String): User? {

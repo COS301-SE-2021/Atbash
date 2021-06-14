2b0e64549f698eeafc1df0e074805127512752eb
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:04:29 +0200

    Added MessageController sendMessage

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/MessageController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/MessageController.kt
@@ -1,10 +1,25 @@
 package za.ac.up.cs.atbash.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import za.ac.up.cs.atbash.json.message.SendMessageJson
+import za.ac.up.cs.atbash.json.message.SendMessageResponseJson
 import za.ac.up.cs.atbash.service.MessageService
 
 @RestController
 class MessageController(@Autowired private val messageService: MessageService) {
 
+    @PostMapping(path = ["rs/v1/messages"])
+    fun sendMessage(@RequestBody json: SendMessageJson): ResponseEntity<SendMessageResponseJson> {
+        val successful = messageService.sendMessage(json.to, json.contents)
+        return if(successful) {
+            ResponseEntity.status(HttpStatus.OK).body(SendMessageResponseJson(true))
+        } else {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(SendMessageResponseJson(false))
+        }
+    }
 }

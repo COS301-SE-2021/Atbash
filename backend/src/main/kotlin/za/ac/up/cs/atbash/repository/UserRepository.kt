9efdcfaf404812aec8e8d9eb18c473e17603034b
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:56:17 +0200

    Added UserRepository interface

---
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package za.ac.up.cs.atbash.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import za.ac.up.cs.atbash.domain.User
+
+@Repository
+interface UserRepository: JpaRepository<User, String> {
+}

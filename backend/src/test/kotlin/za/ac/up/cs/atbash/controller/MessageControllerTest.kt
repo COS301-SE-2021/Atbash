0e7eff357c75bc02fdbad558b3a22737b5aae43c
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:51:52 +0200

    Added Mockito to MessageControllerTest

--- backend/src/test/kotlin/za/ac/up/cs/atbash/controller/MessageControllerTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/controller/MessageControllerTest.kt
@@ -1,5 +1,9 @@
 package za.ac.up.cs.atbash.controller
 
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
 class MessageControllerTest {
 
 }

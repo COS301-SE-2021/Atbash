4dfe4d128a4b33fa57a457276209b368408d47ac
ConnorMayston <cmayston0911@gmail.com>
2021-06-14 22:17:06 +0200

    Finished registerUser function in UserService.kt

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
@@ -5,12 +5,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import za.ac.up.cs.atbash.domain.User
 import za.ac.up.cs.atbash.repository.UserRepository
+import java.util.*
 
 @Service
 class UserService(@Autowired private val userRepository: UserRepository) {
 
-    fun registerUser(number: String, password: String): User? {
-        return null
+    fun registerUser(number: String, password: String): Boolean {
+        return if(userRepository.findByNumber(number) == null){
+            userRepository.save(User(number, UUID.randomUUID().toString(), BCryptPasswordEncoder().encode(password)))
+            true
+        }else{
+            false
+        }
+
     }
 
     fun verifyLogin(number: String, password: String): String? {

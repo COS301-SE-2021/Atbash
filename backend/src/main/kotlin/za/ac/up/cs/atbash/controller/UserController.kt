6a296e43643cd8ee81e068484a4a927b190b46c8
ConnorMayston <cmayston0911@gmail.com>
2021-06-14 16:02:02 +0200

    Added parameters and return type to login in UserController.kt

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
@@ -1,15 +1,20 @@
 package za.ac.up.cs.atbash.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import za.ac.up.cs.atbash.json.user.LoginRequestJson
 import za.ac.up.cs.atbash.service.UserService
 
 @RestController
 class UserController(@Autowired private val userService: UserService){
 
     @PostMapping(path = ["rs/v1/login"])
-    fun login(){
-
+    fun login(@RequestBody json: LoginRequestJson): ResponseEntity<Unit>{
+        return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
     }
+
 }

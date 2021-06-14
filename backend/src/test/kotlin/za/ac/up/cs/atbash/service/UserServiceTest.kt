445e65c1ad75127c71ce49f39f8790038bb7da9c
DYLERN <dylan@squarepeg.co.za>
2021-06-14 10:15:48 +0200

    Added mocks to UserServiceTest

--- backend/src/test/kotlin/za/ac/up/cs/atbash/service/UserServiceTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/service/UserServiceTest.kt
@@ -1,8 +1,17 @@
 package za.ac.up.cs.atbash.service
 
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import za.ac.up.cs.atbash.repository.UserRepository
 
 @ExtendWith(MockitoExtension::class)
 class UserServiceTest {
+
+    @Mock
+    private lateinit var userRepository: UserRepository
+
+    @InjectMocks
+    private lateinit var userService: UserService
 }

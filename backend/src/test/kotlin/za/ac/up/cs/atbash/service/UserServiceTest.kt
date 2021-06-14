22d233bd11b08148e771f505d72f0066651fe3af
DYLERN <dylan@squarepeg.co.za>
2021-06-14 10:23:21 +0200

    Added getUserByNumberReturnsMatch test

--- backend/src/test/kotlin/za/ac/up/cs/atbash/service/UserServiceTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/service/UserServiceTest.kt
@@ -1,9 +1,13 @@
 package za.ac.up.cs.atbash.service
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import za.ac.up.cs.atbash.domain.User
 import za.ac.up.cs.atbash.repository.UserRepository
 
 @ExtendWith(MockitoExtension::class)
@@ -14,4 +18,11 @@ class UserServiceTest {
 
     @InjectMocks
     private lateinit var userService: UserService
+
+    @Test
+    fun getUserByNumberReturnsMatch() {
+        Mockito.`when`(userRepository.findByNumber("123")).thenReturn(User("123"))
+        val user = userService.getUserByNumber("123")
+        Assertions.assertNotNull(user)
+    }
 }

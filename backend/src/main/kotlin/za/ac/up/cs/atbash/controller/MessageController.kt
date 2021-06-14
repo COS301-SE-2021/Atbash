cb700deb11ddbe0f2c383c28916a702982686e4f
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:02:36 +0200

    Added MessageService dependency to MessageController

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/MessageController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/MessageController.kt
@@ -1,8 +1,10 @@
 package za.ac.up.cs.atbash.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RestController
+import za.ac.up.cs.atbash.service.MessageService
 
 @RestController
-class MessageController {
+class MessageController(@Autowired private val messageService: MessageService) {
 
 }

694c2936d41b49217bcfe03f9b4165cfc5c0d590
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:39:58 +0200

    Added MessageRepository dependency to MessageService

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/MessageService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/MessageService.kt
@@ -1,7 +1,11 @@
 package za.ac.up.cs.atbash.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import za.ac.up.cs.atbash.repository.MessageRepository
 
 @Service
-class MessageService {
+class MessageService(
+    @Autowired private val messageRepository: MessageRepository
+) {
 }

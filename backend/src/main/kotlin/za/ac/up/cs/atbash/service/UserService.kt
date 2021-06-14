8d45d462f5d9399733db4778e4bd6a7a54e53496
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:57:50 +0200

    Added UserRepository dependency to UserService

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
@@ -1,7 +1,9 @@
 package za.ac.up.cs.atbash.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import za.ac.up.cs.atbash.repository.UserRepository
 
 @Service
-class UserService {
+class UserService(@Autowired private val userRepository: UserRepository) {
 }

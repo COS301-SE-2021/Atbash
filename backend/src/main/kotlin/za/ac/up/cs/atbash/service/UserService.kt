bedb0bd64585448d5c37773cb83d78da9ae77851
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:58:44 +0200

    Added getUserByNumber skeleton to UserService

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/UserService.kt
@@ -2,8 +2,13 @@ package za.ac.up.cs.atbash.service
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import za.ac.up.cs.atbash.domain.User
 import za.ac.up.cs.atbash.repository.UserRepository
 
 @Service
 class UserService(@Autowired private val userRepository: UserRepository) {
+
+    fun getUserByNumber(to: String): User? {
+        return null
+    }
 }

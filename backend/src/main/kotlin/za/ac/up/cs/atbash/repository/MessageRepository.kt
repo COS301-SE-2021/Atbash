4f8636282f66ff0dcf2ee1403f482e730921fd4f
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:45:47 +0200

    Changed MessageRepository entity type to Message

--- backend/src/main/kotlin/za/ac/up/cs/atbash/repository/MessageRepository.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/repository/MessageRepository.kt
@@ -2,6 +2,7 @@ package za.ac.up.cs.atbash.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import za.ac.up.cs.atbash.domain.Message
 
 @Repository
-interface MessageRepository: JpaRepository<String, String>
+interface MessageRepository: JpaRepository<Message, String>

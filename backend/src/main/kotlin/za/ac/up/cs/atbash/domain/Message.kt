c112d49c846a8d82e6721662a210c0c1f70c2875
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:12:07 +0200

    Added properties to Message data class

--- backend/src/main/kotlin/za/ac/up/cs/atbash/domain/Message.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/domain/Message.kt
@@ -1,4 +1,22 @@
 package za.ac.up.cs.atbash.domain
 
-class Message {
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.*
+
+data class Message(
+    @Column
+    @ManyToOne(fetch = FetchType.EAGER)
+    val to: User,
+
+    @Column
+    val contents: String
+) {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(length = 36, updatable = false, nullable = false)
+    val id: String = ""
 }

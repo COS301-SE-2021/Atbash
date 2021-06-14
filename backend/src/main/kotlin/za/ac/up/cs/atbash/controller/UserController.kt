7803cb04b20f2f6a3e1fea83d2161b818add3677
ConnorMayston <cmayston0911@gmail.com>
2021-06-14 15:54:42 +0200

    Added UserService dependency to user controller

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
@@ -1,7 +1,10 @@
 package za.ac.up.cs.atbash.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RestController
+import za.ac.up.cs.atbash.service.UserService
 
 @RestController
-class UserController {
+class UserController(@Autowired private val userService: UserService){
+
 }

5f9532b40a0b684549fc4cfdc7837da5efff5a94
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:54:33 +0200

    Added Mockito to MessageServiceTest

--- backend/src/test/kotlin/za/ac/up/cs/atbash/service/MessageServiceTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/service/MessageServiceTest.kt
@@ -1,4 +1,8 @@
 package za.ac.up.cs.atbash.service
 
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
 class MessageServiceTest {
 }

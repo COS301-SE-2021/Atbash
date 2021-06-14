9bb6987074c337ce843a30a4b287797cd0dc0c06
ConnorMayston <cmayston0911@gmail.com>
2021-06-14 15:56:50 +0200

    Created login stub in UserController.kt

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
@@ -1,10 +1,15 @@
 package za.ac.up.cs.atbash.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import za.ac.up.cs.atbash.service.UserService
 
 @RestController
 class UserController(@Autowired private val userService: UserService){
 
+    @PostMapping(path = ["rs/v1/login"])
+    fun login(){
+
+    }
 }

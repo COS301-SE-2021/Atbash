31682b20bdee776f499b2fc785d16029053af7a7
DYLERN <dylan@squarepeg.co.za>
2021-06-14 08:11:55 +0200

    Added properties to User data class

--- backend/src/main/kotlin/za/ac/up/cs/atbash/domain/User.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/domain/User.kt
@@ -1,4 +1,20 @@
 package za.ac.up.cs.atbash.domain
 
-class User {
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+data class User(
+    @Column(unique = true)
+    val number: String
+) {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(length = 36, updatable = false, nullable = false)
+    val id: String = ""
 }

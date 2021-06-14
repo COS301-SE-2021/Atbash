c923bc86726c7585be87329f27ea128f6a002d10
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:52:23 +0200

    Added mocks to MessageControllerTest

--- backend/src/test/kotlin/za/ac/up/cs/atbash/controller/MessageControllerTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/controller/MessageControllerTest.kt
@@ -1,9 +1,16 @@
 package za.ac.up.cs.atbash.controller
 
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import za.ac.up.cs.atbash.service.MessageService
 
 @ExtendWith(MockitoExtension::class)
 class MessageControllerTest {
+    @Mock
+    private lateinit var messageService: MessageService
 
+    @InjectMocks
+    private lateinit var messageController: MessageController
 }

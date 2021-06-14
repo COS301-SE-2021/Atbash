b60af116f3e97b3f00951a7b4fd765d51a3d8f40
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:56:08 +0200

    Added mocks to MessageServiceTest

--- backend/src/test/kotlin/za/ac/up/cs/atbash/service/MessageServiceTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/service/MessageServiceTest.kt
@@ -1,8 +1,21 @@
 package za.ac.up.cs.atbash.service
 
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import za.ac.up.cs.atbash.repository.MessageRepository
+import za.ac.up.cs.atbash.repository.UserRepository
 
 @ExtendWith(MockitoExtension::class)
 class MessageServiceTest {
+    @Mock
+    private lateinit var userRepository: UserRepository
+
+    @Mock
+    private lateinit var messageRepository: MessageRepository
+
+    @InjectMocks
+    private lateinit var messageService: MessageService
+
 }

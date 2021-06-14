4d981ab64862515fe490c98465e1e5fdc2a0bb92
ConnorMayston <cmayston0911@gmail.com>
2021-06-14 16:26:04 +0200

    Finished UserController.kt login

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/UserController.kt
@@ -7,18 +7,25 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import za.ac.up.cs.atbash.json.user.LoginRequestJson
+import za.ac.up.cs.atbash.json.user.LoginResponseJson
 import za.ac.up.cs.atbash.service.UserService
 
 @RestController
-class UserController(@Autowired private val userService: UserService){
+class UserController(@Autowired private val userService: UserService) {
 
     @PostMapping(path = ["rs/v1/login"])
-    fun login(@RequestBody json: LoginRequestJson): ResponseEntity<Unit>{
-        if(json.number == null || json.password == null){
+    fun login(@RequestBody json: LoginRequestJson): ResponseEntity<LoginResponseJson> {
+        if (json.number == null || json.password == null) {
             return ResponseEntity(HttpStatus.BAD_REQUEST)
         }
 
-        return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        val apiKey = userService.verifyLogin(json.number, json.password)
+        return if (apiKey != null) {
+            ResponseEntity.status(HttpStatus.OK).body(LoginResponseJson(apiKey))
+        } else {
+            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+
     }
 
 }

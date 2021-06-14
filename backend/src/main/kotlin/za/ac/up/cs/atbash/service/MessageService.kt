efedf7f2f0ba4bc13c9736ae09ddc12740ba2bf4
DYLERN <dylan@squarepeg.co.za>
2021-06-14 09:00:58 +0200

    Added basic implementation for MessageService sendMessage

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/MessageService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/MessageService.kt
@@ -2,10 +2,25 @@ package za.ac.up.cs.atbash.service
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import za.ac.up.cs.atbash.domain.Message
 import za.ac.up.cs.atbash.repository.MessageRepository
 
 @Service
 class MessageService(
+    @Autowired private val userService: UserService,
     @Autowired private val messageRepository: MessageRepository
 ) {
+
+    fun sendMessage(to: String, contents: String): Boolean {
+        val userTo = userService.getUserByNumber(to) ?: return false
+
+        val message = Message(userTo, contents)
+
+        return try {
+            messageRepository.save(message)
+            true
+        } catch (exception: Exception) {
+            false
+        }
+    }
 }

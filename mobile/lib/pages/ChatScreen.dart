d8d5d4404d2550315adab3db8a14d6f1e89e95a0
DYLERN <dylan@squarepeg.co.za>
2021-06-17 14:17:06 +0200

    Changed ChatPage Widget to conform to new design

--- mobile/lib/pages/ChatScreen.dart
+++ mobile/lib/pages/ChatScreen.dart
@@ -1,187 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/model/SystemModel.dart';
-import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/domain/Contact.dart';
+import 'package:mobile/domain/Message.dart';
+import 'package:mobile/services/MessageService.dart';
+import 'package:mobile/services/UserService.dart';
+import 'package:mobile/widgets/ProfileIcon.dart';
 
-import '../domain/Contact.dart';
-import '../domain/Message.dart';
+class ChatPage extends StatefulWidget {
+  final Contact contact;
 
-//Main widget
-class ChatScreen extends StatelessWidget {
-  final Contact _contact;
+  ChatPage(this.contact);
 
-  ChatScreen(this._contact);
+  @override
+  _ChatPageState createState() => _ChatPageState(this.contact);
+}
+
+class _ChatPageState extends State<ChatPage> {
+  bool loading = true;
+  late final MessageService _messageService;
+
+  final _inputController = TextEditingController();
+  final _scrollController = ScrollController();
+
+  List<Message> _messages = [];
+
+  _ChatPageState(Contact contact) {
+    _messageService = MessageService(contact);
+
+    _messageService.listenForNewMessages((newMessage) {
+      setState(() {
+        _messages.addAll(newMessage);
+        _scrollController.animateTo(
+          0.0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ChatScreenAppBar(_contact),
-      body: SafeArea(
-        child: ChangeNotifierProvider(
-          create: (context) => SystemModel(),
-          child: Column(
-            children: [
-              Flexible(child: MessageList(_contact)),
-              InputBar(_contact)
-            ],
-          ),
-        ),
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Row(
+        children: [
+          EmptyProfileIcon(Colors.black),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                widget.contact.displayName,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
-}
 
-//Widget for list of messages
-class MessageList extends StatefulWidget {
-  final Contact _contact;
-
-  MessageList(this._contact);
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MessageListState(_contact);
-  }
-}
-
-//State for MessageList
-class _MessageListState extends State<MessageList> {
-  final Contact _contact;
-
-  _MessageListState(this._contact);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<SystemModel>(builder: (context, systemModel, child) {
-      return ListView(
-        children: _buildMessages(_contact.chat?.messages ?? [], systemModel),
-      );
-    });
+  SafeArea _buildBody() {
+    return SafeArea(
+      child: Column(
+        children: [
+          Flexible(
+            child: ListView(
+              children: _buildMessages(),
+              controller: _scrollController,
+              reverse: true,
+            ),
+          ),
+          _buildInput()
+        ],
+      ),
+    );
   }
 
-  List<Widget> _buildMessages(
-      List<Message> messageList, SystemModel systemModel) {
-    List<Widget> messageWidgets = [];
-    messageList.forEach(
-        (element) => messageWidgets.add(MessageItem(element, systemModel)));
-    return messageWidgets;
+  List<Align> _buildMessages() {
+    return _messages.reversed.map((m) => _buildMessage(m)).toList();
   }
-}
 
-//Widget for input
-class InputBar extends StatelessWidget {
-  final Contact contact;
-
-  InputBar(this.contact);
-
-  @override
-  Widget build(BuildContext context) {
-    final inputController = TextEditingController();
-
-    return Consumer<SystemModel>(builder: (context, systemModel, child) {
-      return Container(
-          color: Colors.green,
-          child: Row(
-            children: [
-              Expanded(
-                  child: Card(
-                      color: Colors.white54,
-                      child: TextField(
-                        maxLines: 4,
-                        minLines: 1,
-                        controller: inputController,
-                        style: TextStyle(fontSize: 18),
-                      ))),
-              IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {
-                  systemModel.sendMessage(contact, inputController.text);
-                  inputController.text = "";
-                },
-              )
-            ],
-          ));
-    });
-  }
-}
-
-//Widget for each individual message
-class MessageItem extends StatelessWidget {
-  final Message _message;
-  final SystemModel _systemModel;
-
-  MessageItem(this._message, this._systemModel);
-
-  @override
-  Widget build(BuildContext context) {
+  Align _buildMessage(Message message) {
     Alignment alignment = Alignment.centerLeft;
-    EdgeInsets paddingInset = EdgeInsets.fromLTRB(10.0, 0.0, 40.0, 0.0);
+    EdgeInsets padding = EdgeInsets.only(left: 16.0, right: 32.0);
 
-    if (_message.from == _systemModel.userPhoneNumber) {
+    if (message.numberTo == widget.contact.phoneNumber) {
       alignment = Alignment.centerRight;
-      paddingInset = EdgeInsets.fromLTRB(40.0, 0.0, 10.0, 0.0);
+      padding = EdgeInsets.only(left: 32.0, right: 16.0);
     }
 
     return Align(
       alignment: alignment,
       child: Padding(
-        padding: paddingInset,
+        padding: padding,
         child: Wrap(
           children: [
             Card(
-              color: Colors.green,
+              color: Colors.orange,
               child: Padding(
-                padding: EdgeInsets.fromLTRB(16.0, 10.0, 20.0, 10.0),
+                padding: EdgeInsets.all(16.0),
                 child: Text(
-                  _message.contents,
-                  style: TextStyle(fontSize: 18),
+                  message.contents,
+                  style: TextStyle(fontSize: 18.0),
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
     );
   }
-}
 
-//Widget for AppBar
-class ChatScreenAppBar extends AppBar {
-  ChatScreenAppBar(Contact contact)
-      : super(
-          title: Row(
-            children: [
-              EmptyProfileIcon(Colors.white),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                    contact.displayName,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              )
-            ],
-          ),
-        );
-}
-
-//Widget for profile picture
-class EmptyProfileIcon extends StatelessWidget {
-  final Color _color;
-
-  EmptyProfileIcon(this._color);
-
-  @override
-  Widget build(BuildContext context) {
+  Container _buildInput() {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        border: Border.all(width: 1, color: _color),
-      ),
-      padding: EdgeInsets.all(4.0),
-      child: Icon(
-        Icons.person,
-        color: _color,
+      color: Colors.orange,
+      child: Row(
+        children: [
+          Expanded(
+            child: Card(
+              color: Colors.white54,
+              child: TextField(
+                maxLines: 4,
+                minLines: 1,
+                controller: _inputController,
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: _sendMessage,
+            icon: Icon(Icons.send),
+          )
+        ],
       ),
     );
+  }
+
+  void _sendMessage() {
+    final from = GetIt.I.get<UserService>().getUser()?.phoneNumber;
+    final to = widget.contact.phoneNumber;
+    final contents = _inputController.text;
+    if (from != null) {
+      _messageService.sendMessage(from, to, contents);
+    }
   }
 }

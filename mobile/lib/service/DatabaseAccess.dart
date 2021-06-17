80f3afebd34c7358e92d709df56c164523b0c1ec
DYLERN <dylan@squarepeg.co.za>
2021-06-17 14:07:20 +0200

    Added required functions to DatabaseAccess class

--- mobile/lib/service/DatabaseAccess.dart
+++ mobile/lib/service/DatabaseAccess.dart
@@ -1,3 +1,5 @@
+import 'package:mobile/domain/Contact.dart';
+import 'package:mobile/domain/Message.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -9,10 +11,62 @@ class DatabaseAccess {
   static Future<Database> _init() async {
     String path = join(await getDatabasesPath(), "atbash.db");
     return openDatabase(path, version: 1, onCreate: (db, version) {
-      return db.execute(
+      db.execute(
         """
-        """,
+          create table message (
+            "id" text primary key,
+            "number_from" text not null,
+            "number_to" text not null,
+            "contents" text not null
+          );   
+          """,
       );
+
+      db.execute(
+        """
+          create table contact (
+            "phone_number" text primary key,
+            "display_name" text not null,
+            "has_chat" tinyint not null
+          );
+          """,
+      ).then((value) {
+        db.execute(
+          """
+            insert into contact
+            values
+              ('011 123 1234', 'Liam', 1), 
+              ('021 123 4567', 'Connor', 1), 
+              ('031 456 1235', 'Josh', 0), 
+              ('041 456 4567', 'Targo', 0)
+            ;
+            """,
+        );
+      });
     });
+  }
+
+  Message saveMessage(String from, String to, String contents) {
+    return Message("", from, to, contents);
+  }
+
+  Future<List<Message>> getChatWithContact(String phoneNumber) async {
+    return [];
+  }
+
+  Future<Contact?> fetchContact(String number) async {
+    return null;
+  }
+
+  Contact saveContact(String number, String displayName) {
+    return Contact(number, displayName, false);
+  }
+
+  Future<List<Contact>> getContacts() async {
+    return [];
+  }
+
+  Future<List<Contact>> getContactsWithChats() async {
+    return [];
   }
 }

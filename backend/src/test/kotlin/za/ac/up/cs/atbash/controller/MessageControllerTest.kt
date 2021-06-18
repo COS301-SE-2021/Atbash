6aede18ebcc24dffeafb9b093d62d6a37eaabed4
DYLERN <dylan@squarepeg.co.za>
2021-06-18 18:25:28 +0200

    Updated MessageControllerTests

--- backend/src/test/kotlin/za/ac/up/cs/atbash/controller/MessageControllerTest.kt
+++ backend/src/test/kotlin/za/ac/up/cs/atbash/controller/MessageControllerTest.kt
@@ -22,84 +22,41 @@ class MessageControllerTest {
     private lateinit var messageController: MessageController
 
     @Test
-    @DisplayName("When request body `from` is null, response status should be BAD_REQUEST")
-    fun sendMessageWhenRequestFromNull() {
-        val response = messageController.sendMessage(
-            SendMessageRequestJson(
-                null,
-                "some to",
-                SendMessageRequestJsonContents("", "")
-            )
-        )
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
+    @DisplayName("When authorization header is not present, response status should be UNAUTHORIZED")
+    fun sendMessageWhenAuthorizationNull() {
+        val response = messageController.sendMessage(null, "", "")
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
     }
 
     @Test
     @DisplayName("When request body `to` is null, response status should be BAD_REQUEST")
     fun sendMessageWhenRequestToNull() {
-        val response = messageController.sendMessage(
-            SendMessageRequestJson(
-                "some from",
-                null,
-                SendMessageRequestJsonContents("", "")
-            )
-        )
+        val response = messageController.sendMessage("", null, "")
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
     }
 
     @Test
     @DisplayName("When request body `contents` is null, response status should be BAD_REQUEST")
     fun sendMessageWhenRequestContentsNull() {
-        val response = messageController.sendMessage(SendMessageRequestJson("some from", "some number", null))
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
-    }
-
-    @Test
-    @DisplayName("When request body `contents`:`id` is null, response status should be BAD_REQUEST")
-    fun sendMessageWhenRequestContentsIdNull() {
-        val response =
-            messageController.sendMessage(SendMessageRequestJson("", "", SendMessageRequestJsonContents(null, "")))
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
-    }
-
-    @Test
-    @DisplayName("When request body `contents`:`contents` is null, response status should be BAD_REQUEST")
-    fun sendMessageWhenRequestContentsContentsNull() {
-        val response =
-            messageController.sendMessage(SendMessageRequestJson("", "", SendMessageRequestJsonContents("", null)))
+        val response = messageController.sendMessage("", "", null)
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
     }
 
     @Test
     @DisplayName("When service sendMessage returns true, response status should be OK")
     fun sendMessageWhenServiceSuccessful() {
-        Mockito.`when`(
-            messageService.sendMessage(
-                Mockito.anyString(),
-                Mockito.anyString(),
-                Mockito.anyString(),
-                Mockito.anyString()
-            )
-        )
+        Mockito.`when`(messageService.sendMessage(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
             .thenReturn(true)
-        val response =
-            messageController.sendMessage(SendMessageRequestJson("", "", SendMessageRequestJsonContents("", "")))
+        val response = messageController.sendMessage("", "", "")
         Assertions.assertEquals(HttpStatus.OK, response.statusCode)
     }
 
     @Test
     @DisplayName("When service sendMessage returns false, response status should be INTERNAL_SERVER_ERROR")
     fun sendMessageWhenServiceUnsuccessful() {
-        Mockito.`when`(
-            messageService.sendMessage(
-                Mockito.anyString(),
-                Mockito.anyString(),
-                Mockito.anyString(),
-                Mockito.anyString()
-            )
-        ).thenReturn(false)
-        val response =
-            messageController.sendMessage(SendMessageRequestJson("", "", SendMessageRequestJsonContents("", "")))
+        Mockito.`when`(messageService.sendMessage(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+            .thenReturn(false)
+        val response = messageController.sendMessage("", "", "")
         Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.statusCode)
     }
 }

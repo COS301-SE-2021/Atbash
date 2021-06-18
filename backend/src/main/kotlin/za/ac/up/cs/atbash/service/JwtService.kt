0bdd73d8615f71176c46436a6a4e3400b30879b8
DYLERN <dylan@squarepeg.co.za>
2021-06-18 15:17:34 +0200

    Implemented JwtService verify

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/JwtService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/JwtService.kt
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.security.SignatureException
 import javax.crypto.spec.SecretKeySpec
 
 @Service
@@ -15,5 +16,16 @@ class JwtService {
     fun encode(payload: String): String {
         val key = SecretKeySpec(jwtSecret.toByteArray(), SignatureAlgorithm.HS256.jcaName)
         return Jwts.builder().setPayload(payload).signWith(key).compact()
+    }
+
+    fun verify(token: String): Boolean {
+        val key = SecretKeySpec(jwtSecret.toByteArray(), SignatureAlgorithm.HS256.jcaName)
+
+        return try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
+            true
+        } catch (exception: SignatureException) {
+            false
+        }
     }
 }

a5b434944577bf11ac96de9a7605e74f1d4e5f69
DYLERN <dylan@squarepeg.co.za>
2021-06-18 18:19:53 +0200

    Changed MessageController to accept x-www-form-urlencoded parameters instead of json

--- backend/src/main/kotlin/za/ac/up/cs/atbash/controller/MessageController.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/controller/MessageController.kt
@@ -15,14 +15,14 @@ import za.ac.up.cs.atbash.service.MessageService
 class MessageController(@Autowired private val messageService: MessageService) {
 
     @PostMapping(path = ["rs/v1/messages"])
-    fun sendMessage(@RequestHeader("Authorization") auth: String, @RequestBody json: SendMessageRequestJson): ResponseEntity<SendMessageResponseJson> {
+    fun sendMessage(@RequestHeader("Authorization") auth: String, to: String?, contents: String?): ResponseEntity<SendMessageResponseJson> {
         val bearer = auth.substringAfter("Bearer ")
 
-        if (json.from == null || json.to == null || json.contents == null || json.contents.id == null || json.contents.contents == null) {
+        if (to == null || contents == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(SendMessageResponseJson(false))
         }
 
-        val successful = messageService.sendMessage(bearer, json.to, json.contents.id, json.contents.contents)
+        val successful = messageService.sendMessage(bearer, to, contents)
         return if (successful) {
             ResponseEntity.status(HttpStatus.OK).body(SendMessageResponseJson(true))
         } else {

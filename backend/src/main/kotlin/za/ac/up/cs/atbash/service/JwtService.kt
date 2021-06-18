3446c840d23f2730767dde9804c07fed4599968e
DYLERN <dylan@squarepeg.co.za>
2021-06-18 15:09:47 +0200

    Added jwt secret as @Value in JwtService

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/JwtService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/JwtService.kt
@@ -1,7 +1,11 @@
 package za.ac.up.cs.atbash.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class JwtService {
+
+    @Value("\${jwt.secret}")
+    private val jwtSecret = ""
 }

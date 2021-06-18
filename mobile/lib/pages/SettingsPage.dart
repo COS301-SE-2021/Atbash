6d8753f1b28974bd62d51ec4aa6c0d4a0ccac89a
Josh-Reddy <josh.reddy01@gmail.com>
2021-06-18 19:46:07 +0200

    re-added image and photograph selectors to the settings page

--- mobile/lib/pages/SettingsPage.dart
+++ mobile/lib/pages/SettingsPage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mobile/services/UserService.dart';
 
 class SettingsPage extends StatelessWidget {
   final _userService = GetIt.I.get<UserService>();
-
+  final picker = ImagePicker();
   final _displayNameController = TextEditingController();
   final _statusController = TextEditingController();
 
@@ -31,6 +32,36 @@ class SettingsPage extends StatelessWidget {
     return Container(
       child: ListView(
         children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 20.0),
+            child: Column(children: [
+              Text(
+                "Change profile picture:",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    iconSize: 50,
+                    icon: Icon(Icons.photo_library),
+                    onPressed: () {
+                      _imgFromGallery();
+                    },
+                    tooltip: "Add image using your gallery.",
+                  ),
+                  IconButton(
+                    iconSize: 50,
+                    icon: Icon(Icons.photo_camera),
+                    onPressed: () {
+                      _imgFromCamera(context);
+                    },
+                    tooltip: "Add image using your camera.",
+                  ),
+                ],
+              ),
+            ]),
+          ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
             child: Column(
@@ -82,5 +113,57 @@ class SettingsPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future _imgFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    // setState(() {
+    //   if (pickedFile != null) {
+    //     _image = File(pickedFile.path);
+    //   } else {
+    //     print('No image selected.');
+    //   }
+    // });
+  }
+
+  Future _imgFromCamera(BuildContext context) async {
+    try {
+      final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+      // setState(() {
+      //   if (pickedFile != null) {
+      //     _image = File(pickedFile.path);
+      //   } else {
+      //     print('No image selected.');
+      //   }
+      // });
+    } catch (e) {
+      showAlertDialog(context);
+    }
+  }
+
+  showAlertDialog(BuildContext context) {
+    // Create button
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          Widget okButton = TextButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          );
+
+          AlertDialog alert = AlertDialog(
+            title: Text("ALERT"),
+            content: Text("Your device does not have a functional camera."),
+            actions: [
+              okButton,
+            ],
+          );
+          return alert;
+        });
   }
 }

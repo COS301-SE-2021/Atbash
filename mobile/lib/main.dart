dfce4e664ea0cdca7cd51568f17888bcf67daae6
DYLERN <dylan@squarepeg.co.za>
2021-06-18 08:40:04 +0200

    Changed main to initialize firebase before building app

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile/pages/LoginPage.dart';
@@ -13,12 +15,25 @@ void main() {
 }
 
 class AtbashApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Atbash",
-      theme: ThemeData(primarySwatch: Colors.orange),
-      home: LoginPage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          FirebaseMessaging.instance.getToken().then((value) => print(value));
+
+          return MaterialApp(
+            title: "Atbash",
+            theme: ThemeData(primarySwatch: Colors.orange),
+            home: LoginPage(),
+          );
+        }
+
+        return Container();
+      },
     );
   }
 }

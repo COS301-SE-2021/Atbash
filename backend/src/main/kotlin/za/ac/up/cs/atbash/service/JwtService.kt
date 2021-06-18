a6d3c6a27c7fbfec8da368d5496a28e4708e5076
DYLERN <dylan@squarepeg.co.za>
2021-06-18 15:13:50 +0200

    Implemented JwtService encode

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/JwtService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/JwtService.kt
@@ -1,11 +1,19 @@
 package za.ac.up.cs.atbash.service
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import javax.crypto.spec.SecretKeySpec
 
 @Service
 class JwtService {
 
     @Value("\${jwt.secret}")
     private val jwtSecret = ""
+
+    fun encode(payload: String): String {
+        val key = SecretKeySpec(jwtSecret.toByteArray(), SignatureAlgorithm.HS256.jcaName)
+        return Jwts.builder().setPayload(payload).signWith(key).compact()
+    }
 }

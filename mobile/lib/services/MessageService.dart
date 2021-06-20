2d2f2153aab7343a0fe811106e402151ab10abf7
DYLERN <dylan@squarepeg.co.za>
2021-06-20 09:30:36 +0200

    Added MessageService fetchUnreadMessages

--- mobile/lib/services/MessageService.dart
+++ mobile/lib/services/MessageService.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobile/domain/Contact.dart';
 import 'package:mobile/domain/Message.dart';
 import 'package:mobile/services/DatabaseAccess.dart';
+import 'package:mobile/services/UserService.dart';
+import 'package:uuid/uuid.dart';
 
 class MessageService {
   final db = GetIt.I.get<DatabaseAccess>();
@@ -35,6 +39,39 @@ class MessageService {
       headers: headers,
       body: body,
     ); //  TODO should use 200 to mark message as sent in db
+  }
+
+  Future<List<Message>> fetchUnreadMessages() async {
+    final storage = FlutterSecureStorage();
+    final bearerToken = await storage.read(key: "bearer_token");
+    final userPhoneNumber = GetIt.I.get<UserService>().getUser()?.phoneNumber;
+
+    if (userPhoneNumber != null) {
+      final url = Uri.parse("http://10.0.2.2:8080/rs/v1/messages");
+      final headers = {"Authorization": "Bearer $bearerToken"};
+
+      final response = await http.get(url, headers: headers);
+      final Iterable body = jsonDecode(response.body);
+
+      final uuid = Uuid();
+      final messages = <Message>[];
+      body.forEach((element) {
+        final m = _parseUnreadMessageMap(uuid.v4(), userPhoneNumber, element);
+        if (m != null) {
+          messages.add(m);
+        }
+      });
+      return messages;
+    } else {
+      return [];
+    }
+  }
+
+  Message? _parseUnreadMessageMap(
+      String id, String to, Map<dynamic, dynamic> map) {
+    final String from = map["fromNumber"];
+    final String contents = map["contents"];
+    return Message(id, from, to, contents);
   }
 
   void _addMessage(Message m) {

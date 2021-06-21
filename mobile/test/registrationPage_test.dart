113ca54e6344b26eefad782f570341e654f2e563
Targo Dove <u15020275@tuks.co.za>
2021-06-21 13:31:02 +0200

    Fixed some of the errors in registrationPage_test class

--- mobile/test/registrationPage_test.dart
+++ mobile/test/registrationPage_test.dart
@@ -1,9 +1,47 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:mobile/pages/LoginPage.dart';
 import 'package:mobile/pages/RegistrationPage.dart';
+import 'package:mobile/services/UserService.dart';
+import 'package:mockito/mockito.dart';
+import 'firebaseMock.dart';
+
+class MockUserService extends Mock implements UserService {
+  @override
+  Future<bool> register(
+      String number, String deviceToken, String password){
+    return Future.delayed(
+      Duration(milliseconds: 50),
+          () => true,
+    );
+  }
+}
+class MockFirebaseMessaging extends Mock implements FirebaseMessaging {
+
+  @override
+  Future<String?> getToken({String? vapidKey}){
+    return Future.delayed(
+      Duration(milliseconds: 50),
+          () => "12345",
+    );
+  }
+}
 
 void main() {
+  // TestWidgetsFlutterBinding.ensureInitialized(); Gets called in setupFirebaseAuthMocks()
+  setupFirebaseAuthMocks(); //https://stackoverflow.com/questions/63662031/how-to-mock-the-firebaseapp-in-flutter
+  Firebase.initializeApp();
+  final MockUserService mockUserService = MockUserService();
+  final MockFirebaseMessaging mockFirebaseMessaging = MockFirebaseMessaging();
+
+  GetIt.I.registerSingleton<UserService>(MockUserService());
+  //setUp((){}); Called before every test
+  //tearDown((){}); Called after every test
 
   testWidgets('Check for existence of correct widgets on Registration Page', (WidgetTester tester) async {
     // Build a MaterialApp with the LoginPage.
@@ -17,19 +55,19 @@ void main() {
   });
 
   //need to mock User Service https://pub.dev/packages/mockito
-  // testWidgets('Check for correct widget functionality on Registration Page', (WidgetTester tester) async {
-  //   // Build a MaterialApp with the LoginPage.
-  //   await tester.pumpWidget(MaterialApp(home: RegistrationPage()));
-  //
-  //   //Verify clicking on REGISTER button results in page navigation
-  //   expect(find.text('LOGIN'), findsNothing);
-  //   await tester.enterText(find.byType(TextField).at(0), 'name');
-  //   await tester.enterText(find.byType(TextField).at(1), 'name');
-  //   await tester.enterText(find.byType(TextField).at(2), 'name');
-  //   await tester.pumpAndSettle();
-  //   await tester.tap(find.text('REGISTER'));
-  //   await tester.pumpAndSettle();
-  //   expect(find.text('REGISTER'), findsOneWidget);
-  //   expect(find.text('LOGIN'), findsOneWidget);
-  // });
+  testWidgets('Check for correct widget functionality on Registration Page', (WidgetTester tester) async {
+    // Build a MaterialApp with the LoginPage.
+    await tester.pumpWidget(MaterialApp(home: RegistrationPage()));
+
+    //Verify clicking on REGISTER button results in page navigation
+    expect(find.text('LOGIN'), findsNothing);
+    await tester.enterText(find.byType(TextField).at(0), 'name');
+    await tester.enterText(find.byType(TextField).at(1), 'name');
+    await tester.enterText(find.byType(TextField).at(2), 'name');
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('REGISTER'));
+    await tester.pumpAndSettle();
+    expect(find.text('REGISTER'), findsOneWidget);
+    expect(find.text('LOGIN'), findsOneWidget);
+  });
 }

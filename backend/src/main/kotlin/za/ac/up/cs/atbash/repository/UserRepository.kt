de73b50b4cef5006a28a9bb421a112994c29c70d
DYLERN <dylan@squarepeg.co.za>
2021-06-27 14:52:21 +0200

    Changed UserRepository to handle Java domain classes and MongoDB

--- backend/src/main/kotlin/za/ac/up/cs/atbash/repository/UserRepository.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/repository/UserRepository.kt
@@ -1,10 +1,10 @@
 package za.ac.up.cs.atbash.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 import za.ac.up.cs.atbash.domain.User
 
 @Repository
-interface UserRepository: JpaRepository<User, String> {
-    fun findByNumber(number: String): User?
+interface UserRepository: MongoRepository<User, String> {
+    fun findByPhoneNumber(number: String): User?
 }

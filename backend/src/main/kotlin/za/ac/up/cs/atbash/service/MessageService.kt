2f0ad849b32197602836bb3cbc70cd8acfc67575
DYLERN <dylan@squarepeg.co.za>
2021-06-29 17:00:50 +0200

    Updated MessageService to replace sendMessage with saveMessage

--- backend/src/main/kotlin/za/ac/up/cs/atbash/service/MessageService.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/service/MessageService.kt
@@ -3,45 +3,28 @@ package za.ac.up.cs.atbash.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import za.ac.up.cs.atbash.domain.Message
-import za.ac.up.cs.atbash.dto.UnreadMessageDto
+import za.ac.up.cs.atbash.dto.MessageDto
 import za.ac.up.cs.atbash.repository.MessageRepository
 import java.util.*
 
 @Service
 class MessageService(
     @Autowired private val userService: UserService,
-    @Autowired private val messageRepository: MessageRepository,
-    @Autowired private val jwtService: JwtService,
-    @Autowired private val notificationService: NotificationService
+    @Autowired private val messageRepository: MessageRepository
 ) {
 
-    fun sendMessage(bearer: String, to: String, contents: String, timestamp: Long): Boolean {
-        val tokenPayload = jwtService.parseToken(bearer)
-
-        val fromNumber = tokenPayload?.get("number").toString()
-
-        val userFrom = userService.getUserByNumber(fromNumber) ?: return false
-        val userTo = userService.getUserByNumber(to) ?: return false
+    fun saveMessage(from: String, to: String, contents: String, timestamp: Long): Message? {
+        val userFrom = userService.getUserByNumber(from) ?: return null
+        val userTo = userService.getUserByNumber(to) ?: return null
 
         val message = Message(userFrom, userTo, contents, Date(timestamp))
-
-        return try {
-            messageRepository.save(message)
-            notificationService.notifyUserOfMessage(userTo, userFrom)
-            true
-        } catch (exception: Exception) {
-            false
-        }
+        return messageRepository.save(message)
     }
 
-    fun getUnreadMessages(bearer: String): List<UnreadMessageDto>? {
-        val tokenPayload = jwtService.parseToken(bearer)
-
-        val userNumber = tokenPayload?.get("number").toString()
-
+    fun getUnreadMessages(forNumber: String): List<MessageDto>? {
         return try {
-            val messages = messageRepository.findAllByToNumber(userNumber)
-            messages.map { UnreadMessageDto(it.from.number, it.contents, it.timestamp.time) }
+            val messages = messageRepository.findAllByToNumber(forNumber)
+            messages.map { MessageDto(it.from.number, it.contents, it.timestamp.time) }
         } catch (exception: Exception) {
             null
         }

d369da8769ecab4bb72faacdcc8771e122c38a2f
DYLERN <dylan@squarepeg.co.za>
2021-06-29 17:04:06 +0200

    Updated WebSocketConfig to reflect ChatHandler name change

--- backend/src/main/kotlin/za/ac/up/cs/atbash/config/WebSocketConfig.kt
+++ backend/src/main/kotlin/za/ac/up/cs/atbash/config/WebSocketConfig.kt
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
-import za.ac.up.cs.atbash.handler.ChatWebSocketHandler
+import za.ac.up.cs.atbash.handler.ChatHandler
 
 @Configuration
 @EnableWebSocket
-class WebSocketConfig(@Autowired private val chatWebSocketHandler: ChatWebSocketHandler) : WebSocketConfigurer {
+class WebSocketConfig(@Autowired private val chatHandler: ChatHandler) : WebSocketConfigurer {
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(chatWebSocketHandler, "/chat")
+        registry.addHandler(chatHandler, "/chat")
     }
 }

804cd1663237369845ea3c14364db1f6844984f4
DYLERN <dylan@squarepeg.co.za>
2021-07-01 08:47:12 +0200

    Changed Contact fromMap to use column name constants

--- mobile/lib/domain/Contact.dart
+++ mobile/lib/domain/Contact.dart
@@ -13,12 +13,16 @@ class Contact {
     };
   }
 
-  static Contact fromMap(Map<String, dynamic> map) {
-    final number = map["phone_number"] as String;
-    final displayName = map["display_name"] as String;
-    final hasChat = (map["has_chat"] as int) != 0;
+  static Contact? fromMap(Map<String, Object?> map) {
+    final phoneNumber = map[Contact.COLUMN_PHONE_NUMBER];
+    final displayName = map[Contact.COLUMN_DISPLAY_NAME];
+    final hasChat = map[Contact.COLUMN_HAS_CHAT];
 
-    return Contact(number, displayName, hasChat);
+    if (phoneNumber is String && displayName is String && hasChat is int) {
+      return Contact(phoneNumber, displayName, hasChat == 1);
+    } else {
+      return null;
+    }
   }
 
   static const String TABLE_NAME = "contact";

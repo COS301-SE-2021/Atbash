8eb8fc8495d0698172cf153d3ca2364239504eb9
DYLERN <dylan@squarepeg.co.za>
2021-07-05 13:26:31 +0200

    Added and implemented showNewContactDialog function

--- mobile/lib/dialogs/NewContactDialog.dart
+++ mobile/lib/dialogs/NewContactDialog.dart
@@ -1,6 +1,22 @@
+import 'package:flutter/material.dart';
+
 class PhoneNumberNamePair {
   final String phoneNumber;
   final String name;
 
   PhoneNumberNamePair(this.phoneNumber, this.name);
+}
+
+Future<PhoneNumberNamePair?> showNewContactDialog(BuildContext context) {
+  return showDialog<PhoneNumberNamePair>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => _NewContactDialog());
+}
+
+class _NewContactDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }

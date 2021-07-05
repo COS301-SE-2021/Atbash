65b84cf0966053449addc4b998ede4c55e1a2be9
DYLERN <dylan@squarepeg.co.za>
2021-07-05 13:12:25 +0200

    Created CircledIcon widget

---
+++ mobile/lib/widgets/CircledIcon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CircledIcon extends StatelessWidget {
+  const CircledIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

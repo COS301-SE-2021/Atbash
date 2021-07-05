26ccbc0aa83a4e03513888861021b47c56d8ddb7
DYLERN <dylan@squarepeg.co.za>
2021-07-05 13:15:30 +0200

    Added CircledIcon implementation

--- mobile/lib/widgets/CircledIcon.dart
+++ mobile/lib/widgets/CircledIcon.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CircledIcon extends StatelessWidget {
-  const CircledIcon({Key? key}) : super(key: key);
+  final Color _color;
+  final IconData _icon;
+
+  CircledIcon(this._color, this._icon);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        border: Border.all(width: 1, color: _color),
+      ),
+      padding: EdgeInsets.all(4.0),
+      child: Icon(
+        _icon,
+        color: _color,
+      ),
+    );
   }
 }

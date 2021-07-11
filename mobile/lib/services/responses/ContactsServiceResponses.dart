f529512bd4aa02930973c9529036584726eb3126
DYLERN <dylan@squarepeg.co.za>
2021-07-11 16:23:40 +0200

    Added ContactsServiceResponses.dart

---
+++ mobile/lib/services/responses/ContactsServiceResponses.dart
@@ -0,0 +1,10 @@
+import 'package:mobile/domain/Contact.dart';
+
+class AddContactResponse {
+  final AddContactResponseStatus status;
+  final Contact? contact;
+
+  AddContactResponse(this.status, this.contact);
+}
+
+enum AddContactResponseStatus { SUCCESS, DUPLICATE_NAME, GENERAL_ERROR }

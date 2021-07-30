0c2f8753e7861de11ee6fb42d52ad0d24c13c789
DYLERN <dylan@squarepeg.co.za>
2021-07-30 15:26:04 +0200

    Removed monolith backend

--- backend/src/main/kotlin/za/ac/up/cs/atbash/json/command/ChatCommand.kt
+++
@@ -1,9 +0,0 @@
-package za.ac.up.cs.atbash.json.command
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ChatCommand(
-    val recipientNumber: String,
-    val contents: String
-)

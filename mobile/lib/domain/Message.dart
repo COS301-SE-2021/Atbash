990d2d02c825c684a7221db294bf8103404945f7
DYLERN <dylan@squarepeg.co.za>
2021-08-01 15:25:38 +0200

    Added id field to Message entity

--- mobile/lib/domain/Message.dart
+++ mobile/lib/domain/Message.dart
@@ -1,10 +1,12 @@
 class Message {
+  final String id;
   final String senderPhoneNumber;
   final String recipientPhoneNumber;
   final String contents;
   final DateTime timestamp;
 
   Message(
+    this.id,
     this.senderPhoneNumber,
     this.recipientPhoneNumber,
     this.contents,
@@ -13,6 +15,7 @@ class Message {
 
   Map<String, dynamic> toMap() {
     return {
+      COLUMN_ID: this.id,
       COLUMN_SENDER_PHONE_NUMBER: this.senderPhoneNumber,
       COLUMN_RECIPIENT_PHONE_NUMBER: this.recipientPhoneNumber,
       COLUMN_CONTENTS: this.contents,
@@ -21,16 +24,19 @@ class Message {
   }
 
   static Message? fromMap(Map<String, Object?> map) {
+    final id = map[Message.COLUMN_ID];
     final senderPhoneNumber = map[Message.COLUMN_SENDER_PHONE_NUMBER];
     final recipientPhoneNumber = map[Message.COLUMN_RECIPIENT_PHONE_NUMBER];
     final contents = map[Message.COLUMN_CONTENTS];
     final timestamp = map[Message.COLUMN_TIMESTAMP];
 
-    if (senderPhoneNumber is String &&
+    if (id is String &&
+        senderPhoneNumber is String &&
         recipientPhoneNumber is String &&
         contents is String &&
         timestamp is int) {
       return Message(
+        id,
         senderPhoneNumber,
         recipientPhoneNumber,
         contents,
@@ -42,11 +48,12 @@ class Message {
   }
 
   static const String TABLE_NAME = "message";
+  static const String COLUMN_ID = "id";
   static const String COLUMN_SENDER_PHONE_NUMBER = "sender_number";
   static const String COLUMN_RECIPIENT_PHONE_NUMBER = "recipient_number";
   static const String COLUMN_CONTENTS = "contents";
   static const String COLUMN_TIMESTAMP = "timestamp";
 
   static const String CREATE_TABLE =
-      "create table $TABLE_NAME ($COLUMN_SENDER_PHONE_NUMBER text not null, $COLUMN_RECIPIENT_PHONE_NUMBER text not null, $COLUMN_CONTENTS text not null, $COLUMN_TIMESTAMP int not null);";
+      "create table $TABLE_NAME ($COLUMN_ID text primary key, $COLUMN_SENDER_PHONE_NUMBER text not null, $COLUMN_RECIPIENT_PHONE_NUMBER text not null, $COLUMN_CONTENTS text not null, $COLUMN_TIMESTAMP int not null);";
 }

3c0391af72cd8443a8ba116613e9cc9a62e01feb
ConnorMayston <cmayston0911@gmail.com>
2021-08-02 09:56:47 +0200

    Finished index.js in getusermessages

--- backend/getusermessages/index.js
+++ backend/getusermessages/index.js
@@ -5,24 +5,40 @@ const db = new AWS.DynamoDB.DocumentClient({apiVersion: "2012-08-10", region: pr
 exports.handler = async event => {
 
     const phoneNumber = event.queryStringParameters.phoneNumber
+    let body = [];
 
     if (phoneNumber === undefined) {
         return {statusCode: 400, body: "Phone number not present"}
     }
 
     try {
-        const response = db.scan({
-            TableName: process.env.TABLE_CONNECTIONS,
-            ProjectionExpression: "contents, senderPhoneNumber",
+        const response = await db.scan({
+            TableName: process.env.TABLE_MESSAGES,
             FilterExpression: "recipientPhoneNumber = :n",
             ExpressionAttributeValues: {
                 ":n": phoneNumber
             }
         }).promise()
 
+        if(response.Items.length > 0){
+
+            body = response.Items.map(each => ({
+                id: each.id,
+                senderPhoneNumber: each.senderPhoneNumber,
+                recipientPhoneNumber: each.recipientPhoneNumber,
+                contents: each.contents
+            }))
+
+        }
+
     } catch (error) {
         console.error(error)
         return {statusCode: 500, body: "Database error: " + JSON.stringify(error)}
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(body)
     }
 
 }

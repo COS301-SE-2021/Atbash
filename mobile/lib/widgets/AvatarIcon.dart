611ea6644244d598384954e8c2f088ad1b8fdc26
ConnorMayston <cmayston0911@gmail.com>
2021-08-02 17:31:40 +0200

    Created skeleton for AvatarIcon.dart

---
+++ mobile/lib/widgets/AvatarIcon.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AvatarIcon extends StatelessWidget {
+
+  final String _imageData;
+
+  AvatarIcon(this._imageData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}

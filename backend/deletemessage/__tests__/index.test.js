223703ab252ce2764c7c407f8959f30c486bf791
DYLERN <dylan@squarepeg.co.za>
2021-08-03 20:48:47 +0200

    Grouped tests

--- backend/deletemessage/__tests__/index.test.js
+++ backend/deletemessage/__tests__/index.test.js
@@ -6,38 +6,40 @@ jest.mock("../db_access", () => ({
 const {handler} = require("../index")
 const {existsMessageById, deleteMessageById} = require("../db_access")
 
-test('When handler is called with an undefined id, should return status code 400', () => {
-    handler({pathParameters: {}}).then(response => {
-        expect(response.statusCode).toBe(400);
+describe('unit tests for index.handler', () => {
+    test('When handler is called with an undefined id, should return status code 400', () => {
+        handler({pathParameters: {}}).then(response => {
+            expect(response.statusCode).toBe(400);
+        })
     })
-})
 
-test('When no element with id exists, should return status code 404', async () => {
-    existsMessageById.mockImplementation(() => Promise.resolve(false))
+    test('When no element with id exists, should return status code 404', async () => {
+        existsMessageById.mockImplementation(() => Promise.resolve(false))
 
-    const response = await handler({pathParameters: {id: "123"}})
-    expect(response.statusCode).toBe(404)
-})
+        const response = await handler({pathParameters: {id: "123"}})
+        expect(response.statusCode).toBe(404)
+    })
 
-test('When existsMessageById fails, should return status code 500', async () => {
-    existsMessageById.mockImplementation(() => Promise.reject())
+    test('When existsMessageById fails, should return status code 500', async () => {
+        existsMessageById.mockImplementation(() => Promise.reject())
 
-    const response = await handler({pathParameters: {id: "123"}})
-    expect(response.statusCode).toBe(500)
-})
+        const response = await handler({pathParameters: {id: "123"}})
+        expect(response.statusCode).toBe(500)
+    })
 
-test('When deleteMessageById fails, should return status code 500', async () => {
-    existsMessageById.mockImplementation(() => Promise.resolve(true))
-    deleteMessageById.mockImplementation(() => Promise.reject())
+    test('When deleteMessageById fails, should return status code 500', async () => {
+        existsMessageById.mockImplementation(() => Promise.resolve(true))
+        deleteMessageById.mockImplementation(() => Promise.reject())
 
-    const response = await handler({pathParameters: {id: "123"}})
-    expect(response.statusCode).toBe(500)
-})
+        const response = await handler({pathParameters: {id: "123"}})
+        expect(response.statusCode).toBe(500)
+    })
 
-test('When successful, should return status code 200', async () => {
-    existsMessageById.mockImplementation(() => Promise.resolve(true))
-    deleteMessageById.mockImplementation(() => Promise.resolve())
+    test('When successful, should return status code 200', async () => {
+        existsMessageById.mockImplementation(() => Promise.resolve(true))
+        deleteMessageById.mockImplementation(() => Promise.resolve())
 
-    const response = await handler({pathParameters: {id: "123"}})
-    expect(response.statusCode).toBe(200)
+        const response = await handler({pathParameters: {id: "123"}})
+        expect(response.statusCode).toBe(200)
+    })
 })

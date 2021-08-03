fa835e5ab6ac14b664f91709a218feeb655b9655
DYLERN <dylan@squarepeg.co.za>
2021-08-03 21:14:05 +0200

    Extracted business logic from onconnect handler

--- backend/onconnect/index.js
+++ backend/onconnect/index.js
@@ -1,27 +1,22 @@
-const AWS = require("aws-sdk")
-
-const db = new AWS.DynamoDB.DocumentClient({apiVersion: "2012-08-10", region: process.env.AWS_REGION})
+const {addConnection} = require("db_access")
 
 exports.handler = async event => {
-    console.log("Event is ", event)
-
-    const phoneNumber = event.queryStringParameters.phoneNumber
+    const {phoneNumber} = event.queryStringParameters
+    const {connectionId} = event.requestContext
 
     if (phoneNumber === undefined) {
-        return {statusCode: 400, body: "Phone number not present"}
+        return {statusCode: 400, body: "phoneNumber not present"}
+    }
+
+    if (connectionId === undefined) {
+        return {statusCode: 500, body: "connectionId not present"}
     }
 
     try {
-        await db.put({
-            TableName: process.env.TABLE_CONNECTIONS,
-            Item: {
-                phoneNumber,
-                connectionId: event.requestContext.connectionId
-            }
-        }).promise()
+        await addConnection(phoneNumber, connectionId)
     } catch (error) {
-        return {statusCode: 500, body: "Failed to connect " + JSON.stringify(error)}
+        return {statusCode: 500, body: JSON.stringify(error)}
     }
 
-    return {statusCode: 200, body: "Connected"}
+    return {statusCode: 200, body: "connected"}
 }

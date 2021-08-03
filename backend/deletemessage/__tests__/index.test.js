0a6128fbd008b55654b1218816a91cd26162515e
DYLERN <dylan@squarepeg.co.za>
2021-08-03 21:19:52 +0200

    Changed first test to conform to look of other tests

--- backend/deletemessage/__tests__/index.test.js
+++ backend/deletemessage/__tests__/index.test.js
@@ -7,10 +7,9 @@ const {handler} = require("../index")
 const {existsMessageById, deleteMessageById} = require("../db_access")
 
 describe('unit tests for index.handler', () => {
-    test('When handler is called with an undefined id, should return status code 400', () => {
-        handler({pathParameters: {}}).then(response => {
-            expect(response.statusCode).toBe(400);
-        })
+    test('When handler is called with an undefined id, should return status code 400', async () => {
+        const response = await handler({pathParameters: {}})
+        expect(response.statusCode).toBe(400)
     })
 
     test('When no element with id exists, should return status code 404', async () => {

2b5041d5708707548a3c6b8bcffc8d5d79593c50
DYLERN <dylan@squarepeg.co.za>
2021-08-03 21:43:51 +0200

    Extracted business logic from sendmessage handler

--- backend/sendmessage/index.js
+++ backend/sendmessage/index.js
@@ -1,17 +1,10 @@
-const AWS = require("aws-sdk")
-
-const db = new AWS.DynamoDB.DocumentClient({apiVersion: "2012-08-10", region: process.env.AWS_REGION})
+const {getPhoneNumberOfConnection, saveMessage, getConnectionOfPhoneNumber} = require("db_access")
+const {sendToConnection} = require("api_access")
 
 exports.handler = async event => {
-    console.log("Event is ", event)
+    const {connectionId} = event.requestContext
 
-    const connectionId = event.requestContext.connectionId
-
-    const body = JSON.parse(event.body)
-
-    const id = body.id
-    const recipientPhoneNumber = body.recipientPhoneNumber
-    const contents = body.contents
+    const {id, recipientPhoneNumber, contents} = JSON.parse(event.body)
 
     if (anyUndefined(id, recipientPhoneNumber, contents)) {
         console.log("Bad request: ", id, recipientPhoneNumber, contents)
@@ -21,73 +14,35 @@ exports.handler = async event => {
     let senderPhoneNumber
 
     try {
-        const response = await db.query({
-            TableName: process.env.TABLE_CONNECTIONS,
-            KeyConditionExpression: "connectionId = :c",
-            ExpressionAttributeValues: {
-                ":c": connectionId
-            }
-        }).promise()
-
-        if (response.Items.length === 0) {
-            console.error("No phone number associated with connection")
-            return {statusCode: 500, body: "No phone number associated with connection"}
-        }
-
-        senderPhoneNumber = response.Items[0].phoneNumber
+        senderPhoneNumber = await getPhoneNumberOfConnection(connectionId)
     } catch (error) {
-        console.error(error)
+        console.log(error)
+        return {statusCode: 500, body: JSON.stringify(error)}
+    }
+
+    try {
+        await saveMessage(id, senderPhoneNumber, recipientPhoneNumber, contents)
+    } catch (error) {
+        console.log(error)
         return {statusCode: 500, body: "Database error: " + JSON.stringify(error)}
     }
 
     try {
-        await db.put({
-            TableName: process.env.TABLE_MESSAGES,
-            Item: {
+        const recipientConnection = await getConnectionOfPhoneNumber(recipientPhoneNumber)
+        if (recipientConnection !== undefined) {
+            await sendToConnection(event.requestContext.domainName + "/" + event.requestContext.stage, recipientConnection, {
                 id,
                 senderPhoneNumber,
                 recipientPhoneNumber,
                 contents
-            }
-        }).promise()
-    } catch (error) {
-        console.error(error)
-        return {statusCode: 500, body: "Database error: " + JSON.stringify(error)}
-    }
-
-    try {
-        const response = await db.scan({
-            TableName: process.env.TABLE_CONNECTIONS,
-            FilterExpression: "phoneNumber = :n",
-            ExpressionAttributeValues: {
-                ":n": recipientPhoneNumber
-            }
-        }).promise()
-
-        if (response.Items.length > 0) {
-            const recipientConnectionId = response.Items[0].connectionId.toString()
-
-            const api = new AWS.ApiGatewayManagementApi({
-                apiVersion: "2018-11-29",
-                endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
             })
-
-            await api.postToConnection({
-                ConnectionId: recipientConnectionId,
-                Data: JSON.stringify({
-                    id,
-                    senderPhoneNumber,
-                    recipientPhoneNumber,
-                    contents
-                })
-            }).promise()
         }
     } catch (error) {
-        console.error(error)
-        return {statusCode: 500, body: "Database error: " + JSON.stringify(error)}
+        console.log(error)
+        return {statusCode: 500, body: JSON.stringify(error)}
     }
 
-    return {statusCode: 200, body: "Sent"}
+    return {statusCode: 200, body: "sent"}
 
 }
 

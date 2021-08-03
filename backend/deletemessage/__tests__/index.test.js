7715ae8f0fb00b646cac2fa90e082d5ca8e8a1bb
DYLERN <dylan@squarepeg.co.za>
2021-08-03 20:43:10 +0200

    Added test for non existing id

--- backend/deletemessage/__tests__/index.test.js
+++ backend/deletemessage/__tests__/index.test.js
@@ -1,7 +1,20 @@
+jest.mock("../db_access", () => ({
+    existsMessageById: jest.fn(),
+    deleteMessageById: jest.fn()
+}))
+
 const {handler} = require("../index")
+const {existsMessageById, deleteMessageById} = require("../db_access")
 
 test('When handler is called with an undefined id, should return status code 400', () => {
     handler({pathParameters: {}}).then(response => {
         expect(response.statusCode).toBe(400);
     })
+})
+
+test('When no element with id exists, should return status code 404', async () => {
+    existsMessageById.mockImplementation(() => Promise.resolve(false))
+
+    const response = await handler({pathParameters: {id: "123"}})
+    expect(response.statusCode).toBe(404)
 })

90b0f6084692678b5ebab654e4988528f4c5d0be
ConnorMayston <cmayston0911@gmail.com>
2021-08-04 10:52:54 +0200

    Added "When handler is called with an undefined phoneNumber" test

--- backend/getusermessages/__tests__/index.test.js
+++ backend/getusermessages/__tests__/index.test.js
@@ -5,3 +5,9 @@ jest.mock("../db_access", () => ({
 const {handler} = require("../index")
 const {getMessageForPhoneNumber} = require("../db_access")
 
+describe("Unit tests for index.handler for getusermessages", () => {
+    test("When handler is called with an undefined phoneNumber, should return status code 400", async () => {
+        const response = await handler({queryStringParameters: {}})
+        expect(response.statusCode).toBe(400)
+    })
+})

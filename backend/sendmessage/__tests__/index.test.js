da5f911c5a69786a558b708531495ccb6d720801
ConnorMayston <cmayston0911@gmail.com>
2021-08-04 12:44:06 +0200

    Created "When getPhoneNumberOfConnection succeeds but saveMessage fails" test

--- backend/sendmessage/__tests__/index.test.js
+++ backend/sendmessage/__tests__/index.test.js
@@ -34,4 +34,12 @@ describe("Unit tests for index.handler for sendmessage", () => {
         const response = await handler({requestContext: {connectionId: "123"}, body: JSON.stringify({id: "123", recipientPhoneNumber: "0727654673", contents: "Hello"})})
         expect(response.statusCode).toBe(500)
     })
+
+    test("When getPhoneNumberOfConnection succeeds but saveMessage fails , should return status code 500", async () => {
+        getPhoneNumberOfConnection.mockImplementation( () => Promise.resolve({}))
+        saveMessage.mockImplementation(() => Promise.reject())
+
+        const response = await handler({requestContext: {connectionId: "123"}, body: JSON.stringify({id: "123", recipientPhoneNumber: "0727654673", contents: "Hello"})})
+        expect(response.statusCode).toBe(500)
+    })
 })

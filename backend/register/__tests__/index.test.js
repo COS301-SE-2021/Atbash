71ba2203c788d6c06c7f42a1c79d58d459ea4729
ConnorMayston <cmayston0911@gmail.com>
2021-08-04 12:14:38 +0200

    Created "When handler is called with an undefined deviceToken" test

--- backend/register/__tests__/index.test.js
+++ backend/register/__tests__/index.test.js
@@ -16,5 +16,8 @@ describe("Unit tests for index.handler for register",  () => {
         expect(response.statusCode).toBe(400)
     })
 
-
+    test("When handler is called with an undefined deviceToken, should return status code 400", async () => {
+        const response = await handler({body: JSON.stringify({phoneNumber: "0727654673", rsaPublicKey: "123"})})
+        expect(response.statusCode).toBe(400)
+    })
 })

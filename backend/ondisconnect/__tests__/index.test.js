40b7cec419167f089c3099a504c709cd5eb23829
ConnorMayston <cmayston0911@gmail.com>
2021-08-04 11:34:34 +0200

    Added "When handler is called with an undefined connectionId" test

--- backend/ondisconnect/__tests__/index.test.js
+++ backend/ondisconnect/__tests__/index.test.js
@@ -4,3 +4,10 @@ jest.mock("../db_access", () => ({
 
 const {handler} = require("../index")
 const {removeConnection} = require("../db_access")
+
+describe("Unit tests for index.handler for ondisconnect", () => {
+    test("When handler is called with an undefined connectionId, should return status code 500", async () => {
+        const response = await handler({requestContext: {}})
+        expect(response.statusCode).toBe(500)
+    })
+})

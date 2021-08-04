936109178de123fc357439bf7ec3d4a7dbf246dc
ConnorMayston <cmayston0911@gmail.com>
2021-08-04 11:18:16 +0200

    Added "When handler is called with an undefined phoneNumber" test

--- backend/onconnect/__tests__/index.test.js
+++ backend/onconnect/__tests__/index.test.js
@@ -5,3 +5,9 @@ jest.mock("../db_access", () => ({
 const {handler} = require("../index")
 const {addConnection} = require("../db_access")
 
+describe("Unit tests for index.handler for onconnect", () => {
+    test("When handler is called with an undefined phoneNumber, should return status code 400", async () =>{
+        const response = await handler({queryStringParameters: {}, requestContext: {connectionId: 123}})
+        expect(response.statusCode).toBe(400)
+    })
+})

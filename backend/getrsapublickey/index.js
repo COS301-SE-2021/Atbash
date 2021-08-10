f638bf80dfb9efb1b5f6c2c104ff19730ca7b1f1
DYLERN <dylan@squarepeg.co.za>
2021-08-10 19:26:57 +0200

    Changed incorrect 'index' function to 'handler'

--- backend/getrsapublickey/index.js
+++ backend/getrsapublickey/index.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk")
 
 const db = new AWS.DynamoDB.DocumentClient({apiVersion: "2012-08-10", region: process.env.AWS_REGION})
 
-exports.index = async event => {
+exports.handler = async event => {
     const {phoneNumber} = event.pathParameters
 
     if (phoneNumber === undefined) {

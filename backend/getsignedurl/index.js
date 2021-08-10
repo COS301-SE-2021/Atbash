a0c4806065d5cae579855f8e44dd57af7256c75f
DYLERN <dylan@squarepeg.co.za>
2021-08-10 10:33:10 +0200

    Added index.js

---
+++ backend/getsignedurl/index.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk")
+
+const s3Client = new AWS.S3({apiVersion: "2006-03-01", region: process.env.AWS_REGION})
+
+exports.handler = async event => {
+    const {mediaId, method} = JSON.parse(event.requestContext.body)
+
+    if (anyUndefined(mediaId, method)) {
+        return {statusCode: 400, body: "Missing mediaId or method body parameter"}
+    }
+
+    if (method !== "GET" && method !== "PUT") {
+        return {statusCode: 400, body: "method must be either GET or PUT"}
+    }
+
+    try {
+        const signedURL = s3Client.getSignedUrl("putObject", {
+            Bucket: process.env.MEDIA_BUCKET,
+            Key: mediaId
+        })
+
+        return {statusCode: 200, body: signedURL}
+    } catch (error) {
+        return {statusCode: 500, body: JSON.stringify(error)}
+    }
+}
+
+const anyUndefined = (...args) => {
+    return args.some(arg => arg === undefined)
+}

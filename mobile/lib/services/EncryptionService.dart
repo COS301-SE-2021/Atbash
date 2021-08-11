191d9390557db3a3b9a960b7b989ff965d8dc400
DYLERN <dylan@squarepeg.co.za>
2021-08-11 09:22:31 +0200

    Renamed parameters for clarity

--- mobile/lib/services/EncryptionService.dart
+++ mobile/lib/services/EncryptionService.dart
@@ -5,19 +5,20 @@ import 'package:cryptography/cryptography.dart';
 class EncryptionService {
   final algorithm = Cryptography.instance.aesCtr(macAlgorithm: Hmac.sha256());
 
-  Future<String> encrypt(String message, String base64SharedSecret) async {
+  Future<String> encrypt(String plainText, String base64SharedSecret) async {
     final sharedSecret = SecretKeyData(base64Decode(base64SharedSecret));
 
     final encrypted =
-        await algorithm.encrypt(message.codeUnits, secretKey: sharedSecret);
+        await algorithm.encrypt(plainText.codeUnits, secretKey: sharedSecret);
 
     return String.fromCharCodes(encrypted.concatenation());
   }
 
-  Future<String> decrypt(String message, String base64SharedSecret) async {
+  Future<String> decrypt(
+      String encryptedText, String base64SharedSecret) async {
     final sharedSecret = SecretKeyData(base64Decode(base64SharedSecret));
 
-    final encrypted = SecretBox.fromConcatenation(message.codeUnits,
+    final encrypted = SecretBox.fromConcatenation(encryptedText.codeUnits,
         nonceLength: 16, macLength: 32);
 
     final decrypted =

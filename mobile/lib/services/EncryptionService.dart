77e7729c98839cdc68843a354a01477211e86553
DYLERN <dylan@squarepeg.co.za>
2021-08-11 09:06:19 +0200

    Added encrypt function

--- mobile/lib/services/EncryptionService.dart
+++ mobile/lib/services/EncryptionService.dart
@@ -1,1 +1,16 @@
-class EncryptionService {}
+import 'dart:convert';
+
+import 'package:cryptography/cryptography.dart';
+
+class EncryptionService {
+  Future<String> encrypt(String message, String base64SharedSecret) async {
+    final algorithm = Cryptography.instance.aesCtr(macAlgorithm: Hmac.sha256());
+
+    final sharedSecret = SecretKeyData(base64Decode(base64SharedSecret));
+
+    final encrypted =
+        await algorithm.encrypt(message.codeUnits, secretKey: sharedSecret);
+
+    return String.fromCharCodes(encrypted.cipherText);
+  }
+}

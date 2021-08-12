b31c95635ef8e56b609cbfe3f9ff6f7694f76d6d
Blackbird0911 <liamm0912@gmail.com>
2021-08-12 15:02:50 +0200

    Created ProfileSetupPage.dart

---
+++ mobile_ui/lib/pages/ProfileSetupPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileSetupPage extends StatefulWidget {
+  const ProfileSetupPage({Key? key}) : super(key: key);
+
+  @override
+  _ProfileSetupPageState createState() => _ProfileSetupPageState();
+}
+
+class _ProfileSetupPageState extends State<ProfileSetupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

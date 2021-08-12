8d9b499df1b1d7681d177b6e8a092674abdd836d
Blackbird0911 <liamm0912@gmail.com>
2021-08-12 10:34:17 +0200

    Created scroll bar on top of MainPage to select between normal and private chats.

--- mobile_ui/lib/pages/MainPage.dart
+++ mobile_ui/lib/pages/MainPage.dart
@@ -11,11 +11,78 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(
+              Icons.account_circle_rounded,
+              size: 50,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Liam",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    "Just vibing",
+                    style: TextStyle(
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Column(
           children: [
-            Container(),
+            Container(
+              padding: EdgeInsets.all(10),
+              color: Colors.orange,
+              child: Row(
+                children: [
+                  Container(
+                    child: Text("Chats"),
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Container(
+                    child: Text("Private Chats"),
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               child: ListView.builder(
                 itemBuilder: (context, index) => Container(),

b1139764a060f704c3dc16dc9d9ca34ef9cac9a8
Blackbird0911 <liamm0912@gmail.com>
2021-08-12 13:53:10 +0200

    Created RegistrationPage.dart redesign

--- mobile_ui/lib/pages/RegistrationPage.dart
+++ mobile_ui/lib/pages/RegistrationPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_ui/Domain/constants.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({Key? key}) : super(key: key);
@@ -10,6 +11,72 @@ class RegistrationPage extends StatefulWidget {
 class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(
+            flex: 2,
+          ),
+          Image.asset(
+            "assets/logo.png",
+            width: 256,
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 80),
+            child: Container(
+              padding: EdgeInsets.symmetric(),
+              child: TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Constants.orangeColor,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          RegistrationButton(),
+          Spacer(
+            flex: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RegistrationButton extends StatelessWidget {
+  const RegistrationButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Constants.orangeColor,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        "Register",
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      onPressed: () {},
+    );
   }
 }

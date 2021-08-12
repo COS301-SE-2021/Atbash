13fd04eaf8bc115a199592bf4640a9a7f8fd70c7
Blackbird0911 <liamm0912@gmail.com>
2021-08-12 15:21:02 +0200

    Implemented start of ProfileSetupPage.dart

--- mobile_ui/lib/pages/ProfileSetupPage.dart
+++ mobile_ui/lib/pages/ProfileSetupPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_ui/Domain/constants.dart';
 
 class ProfileSetupPage extends StatefulWidget {
   const ProfileSetupPage({Key? key}) : super(key: key);
@@ -10,6 +11,41 @@ class ProfileSetupPage extends StatefulWidget {
 class _ProfileSetupPageState extends State<ProfileSetupPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Profile info",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Constants.orangeColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Please provide a name as well as an optional profile picture and status.",
+                textAlign: TextAlign.center,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.account_circle,
+                  size: 64,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

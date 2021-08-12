ada714a515c34e8852a5ed891a4a15d2a66f6662
Blackbird0911 <liamm0912@gmail.com>
2021-08-12 10:46:58 +0200

    Created ChatPage AppBar.

--- mobile_ui/lib/pages/ChatPage.dart
+++ mobile_ui/lib/pages/ChatPage.dart
@@ -11,7 +11,45 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SizedBox(
+              width: 20,
+            ),
+            Icon(
+              Icons.circle,
+              size: 40,
+              color: Colors.white,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Liam Mayston",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "Online",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
       body: Column(),
     );
   }

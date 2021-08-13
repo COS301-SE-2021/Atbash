2266ec098f290faa8cfc1377c853f0c8459c5e05
Blackbird0911 <liamm0912@gmail.com>
2021-08-13 10:40:45 +0200

    SettingsPage.dart. Created heading

--- mobile_ui/lib/pages/SettingsPage.dart
+++ mobile_ui/lib/pages/SettingsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_ui/Domain/constants.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -10,6 +11,26 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

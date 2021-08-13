9df8a3fba5d5067faa0e694adda4fa1fd4a79b9a
Blackbird0911 <liamm0912@gmail.com>
2021-08-13 10:52:14 +0200

    ProfileSetupPage.dart. Fixed scroll error.

--- mobile_ui/lib/pages/ProfileSetupPage.dart
+++ mobile_ui/lib/pages/ProfileSetupPage.dart
@@ -16,100 +16,103 @@ class _ProfileSetupPageState extends State<ProfileSetupPage> {
         child: Container(
           alignment: Alignment.center,
           padding: EdgeInsets.all(15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Profile info",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Constants.orangeColor,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "Please provide a name as well as an optional profile picture and status.",
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CircleAvatar(
-                backgroundColor: Constants.orangeColor,
-                radius: 64,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  IconButton(
-                    splashRadius: 24,
-                    color: Colors.black.withOpacity(0.7),
-                    iconSize: 32,
-                    onPressed: () {},
-                    icon: Icon(Icons.photo_library),
+                  Text(
+                    "Profile info",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Constants.orangeColor,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                   SizedBox(
-                    width: 48,
+                    height: 15,
                   ),
-                  IconButton(
-                    splashRadius: 24,
-                    color: Colors.black.withOpacity(0.7),
-                    iconSize: 32,
+                  Text(
+                    "Please provide a name as well as an optional profile picture and status.",
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Constants.orangeColor,
+                    radius: 64,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        splashRadius: 24,
+                        color: Colors.black.withOpacity(0.7),
+                        iconSize: 32,
+                        onPressed: () {},
+                        icon: Icon(Icons.photo_library),
+                      ),
+                      SizedBox(
+                        width: 48,
+                      ),
+                      IconButton(
+                        splashRadius: 24,
+                        color: Colors.black.withOpacity(0.7),
+                        iconSize: 32,
+                        onPressed: () {},
+                        icon: Icon(Icons.photo_camera),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 40,
+                    ),
+                    child: Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Display Name",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 80,
+                    ),
+                    child: Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Status",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: Constants.orangeColor,
                     onPressed: () {},
-                    icon: Icon(Icons.photo_camera),
+                    child: Text("Next"),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 0,
-                  horizontal: 40,
-                ),
-                child: Container(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Display Name",
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 0,
-                  horizontal: 80,
-                ),
-                child: Container(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Status",
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              MaterialButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                color: Constants.orangeColor,
-                onPressed: () {},
-                child: Text("Next"),
-              ),
-            ],
+            ),
           ),
         ),
       ),

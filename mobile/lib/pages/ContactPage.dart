c4801dcba5b49f75a77e4d7308e16f0ca73dbd72
ConnorMayston <cmayston0911@gmail.com>
2021-08-15 20:43:10 +0200

    Basic layout completed.

--- mobile/lib/pages/ContactPage.dart
+++ mobile/lib/pages/ContactPage.dart
@@ -1,24 +1,104 @@
+import 'dart:convert';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/domain/Contact.dart';
+import 'package:mobile/models/ContactsModel.dart';
+import 'package:mobile/util/Extensions.dart';
+import 'package:mobile/constants.dart';
+import 'package:mobile/widgets/AvatarIcon.dart';
 
 class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage(this.contact);
+
   @override
   _ContactPageState createState() => _ContactPageState();
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final ContactsModel _contactsModel = GetIt.I.get();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(context),
-      body: _buildBody(),
+      body: _buildBody(context),
     );
   }
-}
 
-AppBar _buildAppBar(BuildContext context) {
-  return AppBar();
-}
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      //TODO Change isEmpty to isBlank
+      title: Text("Contact Info"),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            "Edit",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 
-Widget _buildBody() {
-  return Column();
+  Widget _buildBody(BuildContext context) {
+    return Column(
+      children: [
+        //Base64Decoder().convert(widget.contact.profileImage),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: AvatarIcon.fromString(
+            widget.contact.profileImage,
+            radius: MediaQuery.of(context).size.width * 0.65,
+          ),
+        ),
+        Divider(
+          thickness: 5.0,
+          color: Colors.black,
+          height: 5.0,
+        ),
+        Row(children: [
+          Text("Display Name:"),
+          Text(widget.contact.displayName),
+        ]),
+        Divider(
+          thickness: 5.0,
+          color: Colors.black,
+          height: 5.0,
+        ),
+        Container(
+          padding: EdgeInsets.zero,
+          color: Constants.orangeColor,
+          child: Row(children: [
+            Expanded(
+              child: Text("Number:"),
+              flex: 1,
+            ),
+            Expanded(
+              child: Text(widget.contact.phoneNumber),
+              flex: 2,
+            ),
+          ]),
+        ),
+        Divider(
+          thickness: 5.0,
+          color: Colors.black,
+          height: 5.0,
+        ),
+        Row(children: [
+          Text("Status:"),
+          Text(widget.contact.status),
+        ]),
+      ],
+    );
+  }
 }

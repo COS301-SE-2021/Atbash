f80b5f0744204729c5ff9c5f7c6d3610c9221d45
Blackbird0911 <liamm0912@gmail.com>
2021-08-16 13:34:53 +0200

    ContactEditPage.dart. Created design.

--- mobile/lib/pages/ContactEditPage.dart
+++ mobile/lib/pages/ContactEditPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/models/ContactsModel.dart';
+import 'package:mobile/models/UserModel.dart';
+
+import '../constants.dart';
 
 class ContactEditPage extends StatefulWidget {
   const ContactEditPage({Key? key}) : super(key: key);
@@ -8,8 +13,60 @@ class ContactEditPage extends StatefulWidget {
 }
 
 class _ContactEditPageState extends State<ContactEditPage> {
+  ContactsModel _contactsModel = GetIt.I.get();
+
+  TextEditingController _displayNameController =
+      TextEditingController(text: "Liam Mayston");
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: Text(
+                "Edit display name:",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 60),
+              alignment: Alignment.center,
+              child: TextField(
+                controller: _displayNameController,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 160),
+              child: MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                color: Constants.orangeColor,
+                onPressed: () {
+                  final displayName = _displayNameController.text;
+                  if (displayName.isNotEmpty) {
+                    //change contact name
+                  }
+
+                  Navigator.pop(context);
+                },
+                child: Text("Save"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

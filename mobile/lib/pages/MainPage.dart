6f823b2d2f766686a834e7dc7604a1f13167cd8d
Blackbird0911 <liamm0912@gmail.com>
2021-08-16 18:41:56 +0200

    Small UI change in Main Appbar

--- mobile/lib/pages/MainPage.dart
+++ mobile/lib/pages/MainPage.dart
@@ -105,7 +105,7 @@ class _MainPageState extends State<MainPage> {
                     return Text(
                       _userModel.status,
                       style: TextStyle(
-                        fontSize: 10,
+                        fontSize: 12,
                       ),
                     );
                   }

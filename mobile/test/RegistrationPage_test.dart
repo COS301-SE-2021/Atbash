facbe65cb72b37cb2f1ebc9dc6ebe83381b95f97
DYLERN <dylan@squarepeg.co.za>
2021-08-18 11:36:00 +0200

    Added test for clicking register when successful

--- mobile/test/RegistrationPage_test.dart
+++ mobile/test/RegistrationPage_test.dart
@@ -1,19 +1,29 @@
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/models/UserModel.dart';
 import 'package:mobile/pages/RegistrationPage.dart';
 
+import 'service/UserModelMock.dart';
 import 'service/UserServiceMock.dart';
 
 void main() {
   UserServiceMock userServiceMock = UserServiceMock();
+  UserModelMock userModelMock = UserModelMock();
+
+  GetIt.I.registerSingleton(userServiceMock);
+  GetIt.I.registerSingleton<UserModel>(userModelMock);
 
   testWidgets("Registration has image, country code, phone number, and button",
       (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(
-        home: RegistrationPage(userService: userServiceMock),
+        home: RegistrationPage(
+          userService: userServiceMock,
+        ),
       ),
     );
     final logoFinder = find.byType(SvgPicture);
@@ -25,5 +35,27 @@ void main() {
     expect(countryCodePickerFinder, findsOneWidget);
     expect(textFieldFinder, findsOneWidget);
     expect(buttonFinder, findsOneWidget);
+  });
+
+  testWidgets("clicking 'REGISTER' shows loading icon",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RegistrationPage(
+          userService: userServiceMock,
+        ),
+      ),
+    );
+    final buttonFinder = find.byType(MaterialButton);
+    final loadingIconFinder = find.byType(SpinKitThreeBounce);
+
+    expect(buttonFinder, findsOneWidget);
+    expect(loadingIconFinder, findsNothing);
+
+    await tester.tap(buttonFinder);
+    await tester.pump();
+
+    expect(buttonFinder, findsNothing);
+    expect(loadingIconFinder, findsOneWidget);
   });
 }

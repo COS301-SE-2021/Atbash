45d92b93f093d2ce6e2be4dcf70c2649ee24b2f7
Josh-Reddy <josh.reddy01@gmail.com>
2021-08-18 18:00:28 +0200

    Created a test to check for a textfield and a button

---
+++ mobile/test/ContactEditPage/ContactEditPage_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/domain/Contact.dart';
+import 'package:mobile/models/ContactsModel.dart';
+import 'package:mobile/pages/ContactEditPage.dart';
+import 'package:mockito/annotations.dart';
+
+import 'ContactEditPage_test.mocks.dart';
+
+@GenerateMocks([ContactsModel])
+void main() {
+  final contactsModel = MockContactsModel();
+  GetIt.I.registerSingleton<ContactsModel>(contactsModel);
+
+  testWidgets("ContactEditPage has a TextField and a button",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ContactEditPage(
+            Contact("0721985674", "Joshua", "Hi", "", false, false, "")),
+      ),
+    );
+
+    final textFieldFinder = find.byType(TextField);
+    final buttonFinder = find.byType(MaterialButton);
+
+    expect(textFieldFinder, findsOneWidget);
+    expect(buttonFinder, findsOneWidget);
+  });
+}

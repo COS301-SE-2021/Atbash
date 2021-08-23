b241f65bcd1df724acfa2bdd26941deab0661632
ConnorMayston <cmayston0911@gmail.com>
2021-08-23 15:13:46 +0200

    Made skeleton of UserModel.dart and generated UserModel.g.dart

---
+++ mobile/lib/models/UserModel.dart
@@ -0,0 +1,24 @@
+import 'package:mobx/mobx.dart';
+
+part 'UserModel.g.dart';
+
+class UserModel = _UserModel with _$UserModel;
+
+abstract class _UserModel with Store{
+
+  String phoneNumber = "";
+  String displayName = "";
+  String status = "";
+  String profileImage = "";
+  String birthday = "";
+
+  @action
+  register(){
+
+  }
+
+  @action
+  isRegistered(){
+
+  }
+}

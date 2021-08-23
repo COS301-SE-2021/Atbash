2975da32b0f6dc06c6f473f165123350e86d6ec6
ConnorMayston <cmayston0911@gmail.com>
2021-08-23 14:51:15 +0200

    Created Contact Entity

---
+++ mobile/lib/domain/Contact.dart
@@ -0,0 +1,14 @@
+class Contact {
+  final String phoneNumber;
+  final String displayName;
+  final String status;
+  final String profileImage;
+  final DateTime birthday;
+
+  Contact(
+      {required this.phoneNumber,
+      required this.displayName,
+      required this.status,
+      required this.profileImage,
+      required this.birthday});
+}

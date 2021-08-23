8c924dc91a38985dfe0bc3b6653f9bdf241f115d
DYLERN <dylan@squarepeg.co.za>
2021-08-23 13:58:30 +0200

    Added ContactInfoPage

---
+++ mobile/lib/pages/ContactInfoPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactInfoPage extends StatefulWidget {
+  const ContactInfoPage({Key? key}) : super(key: key);
+
+  @override
+  _ContactInfoPageState createState() => _ContactInfoPageState();
+}
+
+class _ContactInfoPageState extends State<ContactInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

5549012a30f7456ecd0994c58462e47132aa9a44
DYLERN <dylan@squarepeg.co.za>
2021-08-23 15:02:13 +0200

    Added ObservableChat

---
+++ mobile/lib/observables/ObservableChat.dart
@@ -0,0 +1,29 @@
+import 'package:mobile/domain/Chat.dart';
+import 'package:mobile/domain/Contact.dart';
+import 'package:mobile/domain/Message.dart';
+import 'package:mobx/mobx.dart';
+
+part 'ObservableChat.g.dart';
+
+class ObservableChat = _ObservableChat with _$ObservableChat;
+
+abstract class _ObservableChat with Store {
+  final String id;
+
+  final String contactPhoneNumber;
+
+  @observable
+  Contact? contact;
+
+  final ChatType chatType;
+
+  @observable
+  Message? mostRecentMessage;
+
+  _ObservableChat(Chat c)
+      : this.id = c.id,
+        this.contactPhoneNumber = c.contactPhoneNumber,
+        this.contact = c.contact,
+        this.chatType = c.chatType,
+        this.mostRecentMessage = c.mostRecentMessage;
+}

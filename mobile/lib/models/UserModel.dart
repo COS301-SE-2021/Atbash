7de225698e58dbce6f944b8c6a2d464de8bf5c0d
ConnorMayston <cmayston0911@gmail.com>
2021-08-23 15:32:17 +0200

    Made all fields observable and Created a constructor

--- mobile/lib/models/UserModel.dart
+++ mobile/lib/models/UserModel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mobx/mobx.dart';
 
 part 'UserModel.g.dart';
@@ -5,14 +6,44 @@ part 'UserModel.g.dart';
 class UserModel = _UserModel with _$UserModel;
 
 abstract class _UserModel with Store {
+  final _storage = FlutterSecureStorage();
+
+  @observable
   String phoneNumber = "";
-  String displayName = "";
-  String status = "";
-  String profileImage = "";
-  String birthday = "";
+
+  @observable
+  String? displayName;
+
+  @observable
+  String? status;
+
+  @observable
+  String? profileImage;
+
+  @observable
+  DateTime? birthday;
+
+  _UserModel() {
+    _storage.read(key: "display_name").then((value) {
+      if (value != null) setDisplayName(value);
+    });
+
+    _storage.read(key: "status").then((value) {
+      if (value != null) setStatus(value);
+    });
+
+    _storage.read(key: "profile_image").then((value) {
+      if (value != null) setProfileImage(value);
+    });
+
+    _storage.read(key: "birthday").then((value) {
+      if (value != null)
+        setBirthday(DateTime.fromMillisecondsSinceEpoch(int.parse(value)));
+    });
+  }
 
   @action
-  register(String phoneNumber) {}
+  void register(String phoneNumber) {}
 
   @action
   bool isRegistered() {

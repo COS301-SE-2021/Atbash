f1290e98032d99c55411172edeb41614d15a39b7
Blackbird0911 <liamm0912@gmail.com>
2021-08-23 15:17:51 +0200

    Created SettingsModel.dart.

---
+++ mobile/lib/models/SettingsModel.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+import 'package:mobx/mobx.dart';
+
+part 'SettingsModel.g.dart';
+
+class ChatListModel = _ChatListModel with _$ChatListModel;
+
+abstract class _ChatListModel with Store {}

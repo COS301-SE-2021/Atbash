2fa4b149e298f1fa2ba6a0c3a739fc75744a817b
DYLERN <dylan@squarepeg.co.za>
2021-08-23 18:31:13 +0200

    Added model registration in main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobile/models/ChatListModel.dart';
+import 'package:mobile/models/ContactListModel.dart';
+import 'package:mobile/models/MessagesModel.dart';
+import 'package:mobile/models/SettingsModel.dart';
 import 'package:mobile/pages/RegistrationPage.dart';
 
 import 'models/UserModel.dart';
@@ -10,8 +14,16 @@ void main() async {
   final navigatorKey = GlobalKey<NavigatorState>();
 
   final userModel = UserModel();
+  final settingsModel = SettingsModel();
+  final chatListModel = ChatListModel();
+  final contactListModel = ContactListModel();
+  final messagesModel = MessagesModel();
 
   GetIt.I.registerSingleton(userModel);
+  GetIt.I.registerSingleton(settingsModel);
+  GetIt.I.registerSingleton(chatListModel);
+  GetIt.I.registerSingleton(contactListModel);
+  GetIt.I.registerSingleton(messagesModel);
 
   runApp(AtbashApp(navigatorKey));
 }

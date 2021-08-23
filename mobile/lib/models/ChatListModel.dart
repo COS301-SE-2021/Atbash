ca38672a459fa373802acd52b9c457ed89cb2f12
DYLERN <dylan@squarepeg.co.za>
2021-08-23 15:27:33 +0200

    Added startChatWithContact implementation

--- mobile/lib/models/ChatListModel.dart
+++ mobile/lib/models/ChatListModel.dart
@@ -3,6 +3,7 @@ import 'package:mobile/domain/Contact.dart';
 import 'package:mobile/domain/Message.dart';
 import 'package:mobile/observables/ObservableChat.dart';
 import 'package:mobx/mobx.dart';
+import 'package:uuid/uuid.dart';
 
 part 'ChatListModel.g.dart';
 
@@ -13,7 +14,25 @@ abstract class _ChatListModel with Store {
   ObservableList<ObservableChat> chats = <ObservableChat>[].asObservable();
 
   @action
-  void startChatWithContact(Contact contact, ChatType chatType) {}
+  void startChatWithContact(Contact contact, ChatType chatType) {
+    // If specific type of chat already exists with contact, do nothing and return
+    if (chats.any((element) =>
+        element.contactPhoneNumber == contact.phoneNumber &&
+        element.chatType == chatType)) {
+      return;
+    }
+
+    final chat = Chat(
+      id: Uuid().v4(),
+      contactPhoneNumber: contact.phoneNumber,
+      contact: contact,
+      chatType: chatType,
+    );
+
+    // TODO persist chat
+
+    chats.add(chat.asObservable());
+  }
 
   @action
   void startChatWithPhoneNumber(String phoneNumber, ChatType chatType) {}

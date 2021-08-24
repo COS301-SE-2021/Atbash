35ea392eda0d6bba3565093d2b74f224827b1389
Targo Dove <u15020275@tuks.co.za>
2021-08-24 21:47:16 +0200

    Added ReceivingDataPackage class

---
+++ mobile/lib/encryption/DataPackages/ReceivingDataPackage.dart
@@ -0,0 +1,28 @@
+class ReceivingDataPackage {
+  final String messageId;
+  final String senderNumber;
+  final int timestamp;
+  final String encryptedContents;
+
+  ReceivingDataPackage(this.messageId, this.senderNumber, this.timestamp, this.encryptedContents);
+
+  ReceivingDataPackage.fromJson(Map<String, dynamic> json)
+      : messageId = json['id'],
+        senderNumber = json['senderPhoneNumber'],
+        timestamp = json['timestamp'],
+        encryptedContents = json['contents'];
+
+  Map<String, dynamic> toJson() => {
+    'id': messageId,
+    'senderPhoneNumber': senderNumber,
+    'timestamp': timestamp,
+    'contents': encryptedContents,
+  };
+}
+
+//Decoding
+// Map<String, dynamic> packageMap = jsonDecode(jsonString);
+// var package = ReceivingDataPackage.fromJson(packageMap);
+
+//Encoding
+// String json = jsonEncode(package)

0d01e717d7b0a3777e0d802f8c18fde010e7e4f8
DYLERN <dylan@squarepeg.co.za>
2021-08-24 13:11:41 +0200

    Added SQL for creating Chat entity

--- mobile/lib/domain/Chat.dart
+++ mobile/lib/domain/Chat.dart
@@ -15,6 +15,18 @@ class Chat {
     required this.chatType,
     this.mostRecentMessage,
   });
+
+  static const String TABLE_NAME = "chat";
+  static const String COLUMN_ID = "chat_id";
+  static const String COLUMN_CONTACT_PHONE_NUMBER = "chat_contact_phone_number";
+  static const String COLUMN_CHAT_TYPE = "chat_type";
+  static const String COLUMN_RECENT_MESSAGE_ID = "chat_recent_message_id";
+  static const String CREATE_TABLE = "create table $TABLE_NAME ("
+      "$COLUMN_ID text primary key,"
+      "$COLUMN_CONTACT_PHONE_NUMBER text not null,"
+      "$COLUMN_CHAT_TYPE int not null,"
+      "$COLUMN_RECENT_MESSAGE_ID text"
+      ");";
 }
 
 enum ChatType { general, private }

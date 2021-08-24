f1e8bef99b82a4a4cc34cc966de3ab7f8b521208
DYLERN <dylan@squarepeg.co.za>
2021-08-24 14:26:50 +0200

    Added toMap and fromMap

--- mobile/lib/domain/Contact.dart
+++ mobile/lib/domain/Contact.dart
@@ -5,12 +5,48 @@ class Contact {
   final String profileImage;
   final DateTime? birthday;
 
-  Contact(
-      {required this.phoneNumber,
-      required this.displayName,
-      required this.status,
-      required this.profileImage,
-      this.birthday});
+  Contact({
+    required this.phoneNumber,
+    required this.displayName,
+    required this.status,
+    required this.profileImage,
+    this.birthday,
+  });
+
+  Map<String, Object?> toMap() {
+    return {
+      COLUMN_PHONE_NUMBER: phoneNumber,
+      COLUMN_DISPLAY_NAME: displayName,
+      COLUMN_STATUS: status,
+      COLUMN_PROFILE_IMAGE: profileImage,
+      COLUMN_BIRTHDAY: birthday?.millisecondsSinceEpoch,
+    };
+  }
+
+  static Contact? fromMap(Map<String, Object?> map) {
+    final phoneNumber = map[COLUMN_PHONE_NUMBER] as String?;
+    final displayName = map[COLUMN_DISPLAY_NAME] as String?;
+    final status = map[COLUMN_STATUS] as String?;
+    final profileImage = map[COLUMN_PROFILE_IMAGE] as String?;
+    final birthday = map[COLUMN_BIRTHDAY] as int?;
+
+    if (phoneNumber != null &&
+        displayName != null &&
+        status != null &&
+        profileImage != null) {
+      return Contact(
+        phoneNumber: phoneNumber,
+        displayName: displayName,
+        status: status,
+        profileImage: profileImage,
+        birthday: birthday != null
+            ? DateTime.fromMillisecondsSinceEpoch(birthday)
+            : null,
+      );
+    } else {
+      return null;
+    }
+  }
 
   static const String TABLE_NAME = "contact";
   static const String COLUMN_PHONE_NUMBER = "contact_phone_number";
@@ -22,7 +58,7 @@ class Contact {
       "$COLUMN_PHONE_NUMBER text primary key,"
       "$COLUMN_DISPLAY_NAME text not null,"
       "$COLUMN_STATUS text not null,"
-      "$COLUMN_PROFILE_IMAGE text not null,"
+      "$COLUMN_PROFILE_IMAGE blob not null,"
       "$COLUMN_BIRTHDAY int"
       ");";
 }

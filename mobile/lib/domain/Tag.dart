238bcc4703390c4a8d6378949e90b06786517fba
DYLERN <dylan@squarepeg.co.za>
2021-08-24 13:34:21 +0200

    Added SQL for creating Tag entity

--- mobile/lib/domain/Tag.dart
+++ mobile/lib/domain/Tag.dart
@@ -6,4 +6,12 @@ class Tag {
     required this.id,
     required this.name,
   });
+
+  static const String TABLE_NAME = "tag";
+  static const String COLUMN_ID = "tag_id";
+  static const String COLUMN_NAME = "tag_name";
+  static const String CREATE_TABLE = "create table $TABLE_NAME ("
+      "$COLUMN_ID text primary key,"
+      "$COLUMN_NAME text not null"
+      ");";
 }

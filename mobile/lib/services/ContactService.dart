ea430d91fa7c60af8186cc53fdc23eb16151daca
ConnorMayston <cmayston0911@gmail.com>
2021-08-24 17:49:18 +0200

    Implemented ContactService.dart

--- mobile/lib/services/ContactService.dart
+++ mobile/lib/services/ContactService.dart
@@ -6,15 +6,72 @@ class ContactService {
 
   ContactService(this.databaseService);
 
-  Future<List<Contact>> fetchAll() {
-    throw UnimplementedError();
+  Future<List<Contact>> fetchAll() async {
+    final db = await databaseService.database;
+
+    final response = await db.query(Contact.TABLE_NAME);
+
+    final contacts = <Contact>[];
+    response.forEach((e) {
+      final contact = Contact.fromMap(e);
+
+      if (contact != null) contacts.add(contact);
+    });
+
+    return contacts;
   }
 
-  Future<Contact> save(Contact contact) {
-    throw UnimplementedError();
+  Future<Contact> insert(Contact contact) async {
+    final db = await databaseService.database;
+
+    await db.transaction((txn) async {
+      final existingContact = await txn.query(Contact.TABLE_NAME,
+          where: "${Contact.COLUMN_PHONE_NUMBER} = ?",
+          whereArgs: [contact.phoneNumber]);
+
+      if (existingContact.isNotEmpty)
+        throw DuplicateContactPhoneNumberException();
+
+      await txn.insert(Contact.TABLE_NAME, contact.toMap());
+    });
+
+    return contact;
   }
 
-  Future<void> deleteByPhoneNumber(String phoneNumber) {
-    throw UnimplementedError();
+  Future<Contact> update(Contact contact) async {
+    final db = await databaseService.database;
+
+    await db.transaction((txn) async {
+      final existingContact = await txn.query(Contact.TABLE_NAME,
+          where: "${Contact.COLUMN_PHONE_NUMBER} = ?",
+          whereArgs: [contact.phoneNumber]);
+
+      if (existingContact.isEmpty)
+        throw ContactWithPhoneNumberDoesNotExistException();
+
+      await txn.update(Contact.TABLE_NAME, contact.toMap());
+    });
+
+    return contact;
+  }
+
+  Future<void> deleteByPhoneNumber(String phoneNumber) async {
+    final db = await databaseService.database;
+
+    await db.transaction((txn) async {
+      final existingContact = await txn.query(Contact.TABLE_NAME,
+          where: "${Contact.COLUMN_PHONE_NUMBER} = ?",
+          whereArgs: [phoneNumber]);
+
+      if (existingContact.isEmpty)
+        throw ContactWithPhoneNumberDoesNotExistException();
+
+      await txn.delete(Contact.TABLE_NAME,
+          where: "${Contact.COLUMN_PHONE_NUMBER} =?", whereArgs: [phoneNumber]);
+    });
   }
 }
+
+class DuplicateContactPhoneNumberException implements Exception {}
+
+class ContactWithPhoneNumberDoesNotExistException implements Exception {}

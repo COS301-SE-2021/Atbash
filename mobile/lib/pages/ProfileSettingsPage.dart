e533ecb8fd6c6f8ebe87576c45c1cf1414655d93
Blackbird0911 <liamm0912@gmail.com>
2021-08-24 13:52:05 +0200

    Copied Mobile_UI version over.

--- mobile/lib/pages/ProfileSettingsPage.dart
+++ mobile/lib/pages/ProfileSettingsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/constants.dart';
 
 class ProfileSettingsPage extends StatefulWidget {
   const ProfileSettingsPage({Key? key}) : super(key: key);
@@ -8,8 +9,115 @@ class ProfileSettingsPage extends StatefulWidget {
 }
 
 class _ProfileSettingsPageState extends State<ProfileSettingsPage> {
+  late bool isFirstTime;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(15),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    "Profile info",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Constants.orange,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "Please provide a name as well as an optional profile picture and status.",
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Constants.orange,
+                    radius: 64,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        splashRadius: 24,
+                        color: Colors.black.withOpacity(0.7),
+                        iconSize: 32,
+                        onPressed: () {},
+                        icon: Icon(Icons.photo_library),
+                      ),
+                      SizedBox(
+                        width: 48,
+                      ),
+                      IconButton(
+                        splashRadius: 24,
+                        color: Colors.black.withOpacity(0.7),
+                        iconSize: 32,
+                        onPressed: () {},
+                        icon: Icon(Icons.photo_camera),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 40,
+                    ),
+                    child: Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Display Name",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 80,
+                    ),
+                    child: Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Status",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    color: Constants.orange,
+                    onPressed: () {},
+                    child: Text("Next"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

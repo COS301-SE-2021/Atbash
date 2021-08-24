95d8571a7c2bb3ecb17ef1bd3a24bbe6bd3fed77
Targo Dove <u15020275@tuks.co.za>
2021-08-24 21:45:46 +0200

    Added PreKeyDBRecord class

---
+++ mobile/lib/encryption/PreKeyDBRecord.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+class PreKeyDBRecord {
+  final int preKeyId;
+  final Uint8List serializedKey;
+
+  PreKeyDBRecord(
+      this.preKeyId,
+      this.serializedKey,
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      COLUMN_KEY_ID: this.preKeyId,
+      COLUMN_SERIALIZED_KEY: base64.encode(this.serializedKey)
+    };
+  }
+
+  static PreKeyDBRecord? fromMap(Map<String, Object?> map) {
+    final keyID = map[PreKeyDBRecord.COLUMN_KEY_ID];
+    final keyEncoded = map[PreKeyDBRecord.COLUMN_SERIALIZED_KEY];
+
+    if (keyID is int &&
+        keyEncoded is String) {
+      return PreKeyDBRecord(
+        keyID,
+        base64.decode(keyEncoded),
+      );
+    } else {
+      return null;
+    }
+  }
+
+  static const String TABLE_NAME = "pre_key_db_record";
+  static const String COLUMN_KEY_ID = "key_id";
+  static const String COLUMN_SERIALIZED_KEY = "serialized_key";
+
+  static const String CREATE_TABLE =
+      "create table $TABLE_NAME ($COLUMN_KEY_ID int primary key, $COLUMN_SERIALIZED_KEY text not null);";
+}

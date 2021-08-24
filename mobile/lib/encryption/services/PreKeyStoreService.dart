c9c2f01815c0791b5ee20519c392db6af0f7d0cb
Targo Dove <u15020275@tuks.co.za>
2021-08-24 22:59:27 +0200

    Added fetchPreKey method to PreKeyStoreService

--- mobile/lib/encryption/services/PreKeyStoreService.dart
+++ mobile/lib/encryption/services/PreKeyStoreService.dart
@@ -2,6 +2,8 @@ import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
 
 import 'package:mobile/services/DatabaseService.dart';
 
+import '../PreKeyDBRecord.dart';
+
 class PreKeyStoreService extends PreKeyStore {
   final DatabaseService _databaseService;
 
@@ -14,7 +16,7 @@ class PreKeyStoreService extends PreKeyStore {
   @override
   Future<PreKeyRecord> loadPreKey(int preKeyId) async {
     try {
-      final preKeyRecord = await _databaseService.fetchPreKey(preKeyId);
+      final preKeyRecord = await fetchPreKey(preKeyId);
       if (preKeyRecord == null) {
         throw InvalidKeyIdException('No such prekeyrecord! - $preKeyId');
       }
@@ -38,4 +40,26 @@ class PreKeyStoreService extends PreKeyStore {
   Future<List<PreKeyRecord>> loadPreKeys() async {
     return await _databaseService.fetchPreKeys();
   }
+
+  /// ******************** Database Methods ********************
+
+  ///Fetches PreKey from the database
+  Future<PreKeyRecord?> fetchPreKey(int preKeyId) async {
+    final db = await _databaseService.database;
+    final response = await db.query(
+      PreKeyDBRecord.TABLE_NAME,
+      where: "${PreKeyDBRecord.COLUMN_KEY_ID} = ?",
+      whereArgs: [preKeyId],
+    );
+
+    if (response.isNotEmpty) {
+      final preKey = PreKeyDBRecord.fromMap(response.first);
+      if (preKey != null) {
+        return PreKeyRecord.fromBuffer(preKey.serializedKey);
+      }
+    }
+    return null;
+  }
+
+
 }

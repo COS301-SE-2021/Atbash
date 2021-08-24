8beec2b7396729c29662f49e71ffd08659831023
Targo Dove <u15020275@tuks.co.za>
2021-08-24 22:06:36 +0200

    Added IdentityKeyStoreService class

---
+++ mobile/lib/encryption/services/IdentityKeyStoreService.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+import 'package:collection/collection.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
+
+import 'package:mobile/services/DatabaseService.dart';
+
+class IdentityKeyStoreService extends IdentityKeyStore {
+  final _storage = FlutterSecureStorage();
+  final DatabaseService _databaseService;
+
+  IdentityKeyStoreService(this._databaseService){
+    _storage.read(key: "local_registration_id").then((value) {
+      if(value == null){
+        final registrationId = generateRegistrationId(false);
+        _storage.write(key: "local_registration_id", value: registrationId.toString());
+      }
+    });
+
+    _storage.read(key: "identity_key_pair").then((value) {
+      if(value == null){
+        final identityKeyPair = generateIdentityKeyPair();
+        _storage.write(key: "identity_key_pair", value: base64.encode(identityKeyPair.serialize()));
+      }
+    });
+  }
+
+  @override
+  Future<IdentityKey> getIdentity(SignalProtocolAddress address) async =>
+      _databaseService.fetchTrustedKey(address).then((value) => value!);
+
+  ///The returned value should never be null
+  @override
+  Future<IdentityKeyPair> getIdentityKeyPair() async
+  => _storage.read(key: "identity_key_pair").then((value)
+      => IdentityKeyPair.fromSerialized(base64.decode(value!)));
+
+  ///The returned value should never be null
+  @override
+  Future<int> getLocalRegistrationId() async
+  => _storage.read(key: "local_registration_id").then((value)
+      => int.parse(value!));
+
+  Future<void> setIdentityKPRegistrationId(IdentityKeyPair identityKeyPair, int registrationId) async {
+    Future.wait([
+      _storage.write(key: "identity_key_pair", value: base64.encode(identityKeyPair.serialize())),
+      _storage.write(key: "local_registration_id", value: registrationId.toString()),
+    ]);
+  }
+
+  @override
+  Future<bool> isTrustedIdentity(SignalProtocolAddress address,
+      IdentityKey? identityKey, Direction? direction) async {
+    final trusted = await _databaseService.fetchTrustedKey(address);
+    if (identityKey == null) {
+      return false;
+    }
+    // ignore: avoid_dynamic_calls
+    return trusted == null || ListEquality().equals(trusted.serialize(), identityKey.serialize());
+  }
+
+  @override
+  Future<bool> saveIdentity(
+      SignalProtocolAddress address, IdentityKey? identityKey) async {
+    final existing = await _databaseService.fetchTrustedKey(address);
+    if (identityKey == null) {
+      return false;
+    }
+    if (identityKey != existing) {
+      _databaseService.saveTrustedKey(address, identityKey);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}

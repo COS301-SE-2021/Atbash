90bd921aca8684bda9c455bed6cd43f53ae1cd12
DYLERN <dylan@squarepeg.co.za>
2021-08-24 17:12:35 +0200

    Added CommunicationService interface

---
+++ mobile/lib/services/CommunicationService.dart
@@ -0,0 +1,11 @@
+import 'package:mobile/domain/Message.dart';
+
+class CommunicationService {
+  Stream<Message> listenForMessages() async* {
+    throw UnimplementedError();
+  }
+
+  void sendMessage() {
+    throw UnimplementedError();
+  }
+}

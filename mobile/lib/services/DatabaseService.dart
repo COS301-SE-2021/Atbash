b9c9e8a5cbafd2ca85e2907b495d8c520062dcf5
Targo Dove <u15020275@tuks.co.za>
2021-08-24 22:38:36 +0200

    Updated DatabaseService

--- mobile/lib/services/DatabaseService.dart
+++ mobile/lib/services/DatabaseService.dart
@@ -2,6 +2,10 @@ import 'package:mobile/domain/Chat.dart';
 import 'package:mobile/domain/Contact.dart';
 import 'package:mobile/domain/Message.dart';
 import 'package:mobile/domain/Tag.dart';
+import 'package:mobile/encryption/PreKeyDBRecord.dart';
+import 'package:mobile/encryption/SessionDBRecord.dart';
+import 'package:mobile/encryption/SignedPreKeyDBRecord.dart';
+import 'package:mobile/encryption/TrustedKeyDBRecord.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -27,8 +31,13 @@ class DatabaseService {
       db.execute("drop table if exists ${Message.TABLE_NAME};"),
       db.execute("drop table if exists ${Contact.TABLE_NAME};"),
       db.execute("drop table if exists ${Tag.TABLE_NAME};"),
+      db.execute("drop table if exists ${PreKeyDBRecord.TABLE_NAME};"),
+      db.execute("drop table if exists ${SessionDBRecord.TABLE_NAME};"),
+      db.execute("drop table if exists ${SignedPreKeyDBRecord.TABLE_NAME};"),
+      db.execute("drop table if exists ${TrustedKeyDBRecord.TABLE_NAME};"),
       db.execute(
           "drop table if exists ${Message.TABLE_NAME}_${Tag.TABLE_NAME};")
+
     ]);
   }
 
@@ -38,6 +47,10 @@ class DatabaseService {
       db.execute(Message.CREATE_TABLE),
       db.execute(Contact.CREATE_TABLE),
       db.execute(Tag.CREATE_TABLE),
+      db.execute(PreKeyDBRecord.CREATE_TABLE),
+      db.execute(SessionDBRecord.CREATE_TABLE),
+      db.execute(SignedPreKeyDBRecord.CREATE_TABLE),
+      db.execute(TrustedKeyDBRecord.CREATE_TABLE),
     ]);
 
     await db.execute("create table ${Message.TABLE_NAME}_${Tag.TABLE_NAME} ("

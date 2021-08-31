d80d2aa349a3822d1638610ca02dfb52eb781de0
ConnorMayston <cmayston0911@gmail.com>
2021-08-31 17:44:14 +0200

    Created ProfileSettingsPageModel.dart

---
+++ mobile/lib/models/ProfileSettingsPageModel.dart
@@ -0,0 +1,25 @@
+import 'dart:typed_data';
+
+import 'package:mobx/mobx.dart';
+
+part 'ProfileSettingsPageModel.g.dart';
+
+class ProfileSettingsPageModel = _ProfileSettingsPageModel with _$ProfileSettingsPageModel;
+
+abstract class _ProfileSettingsPageModel with Store{
+  //TODO make not observable?
+  @observable
+  String phoneNumber = "";
+
+  @observable
+  String displayName = "";
+
+  @observable
+  String status = "";
+
+  @observable
+  DateTime? birthday;
+
+  @observable
+  Uint8List? profilePicture;
+}

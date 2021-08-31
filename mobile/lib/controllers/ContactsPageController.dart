dad1c3a204951e6357a80ff77cf70a1d35fb423e
ConnorMayston <cmayston0911@gmail.com>
2021-08-31 15:34:19 +0200

    created startChat and addContact functions

--- mobile/lib/controllers/ContactsPageController.dart
+++ mobile/lib/controllers/ContactsPageController.dart
@@ -1,17 +1,20 @@
 import 'package:get_it/get_it.dart';
+import 'package:mobile/domain/Chat.dart';
 import 'package:mobile/domain/Contact.dart';
+import 'package:mobile/domain/Message.dart';
 import 'package:mobile/models/ContactsPageModel.dart';
+import 'package:mobile/services/ChatService.dart';
 import 'package:mobile/services/ContactService.dart';
+import 'package:uuid/uuid.dart';
 
 class ContactsPageController {
   final ContactService contactService = GetIt.I.get();
+  final ChatService chatService = GetIt.I.get();
 
   final ContactsPageModel model = ContactsPageModel();
 
   ContactsPageController() {
-    contactService.fetchAll().then((contactList) {
-      model.replaceContacts(contactList);
-    });
+    reload();
   }
 
   void deleteContact(String phoneNumber) {
@@ -19,8 +22,38 @@ class ContactsPageController {
     contactService.deleteByPhoneNumber(phoneNumber);
   }
 
-  void addContact(Contact contact) {
+  void addContact(String number, String name) {
+    //TODO Request status & profile image.
+    Contact contact = new Contact(
+        phoneNumber: number, displayName: name, status: "", profileImage: "");
     model.addContact(contact);
     contactService.insert(contact);
+  }
+
+  Future<Chat> startChat(Contact contact, ChatType chatType,
+      {String? id, Message? mostRecentMessage}) async {
+    final chats = await chatService.fetchAll();
+    final index = chats.indexWhere((element) =>
+        element.contactPhoneNumber == contact.phoneNumber &&
+        element.chatType == chatType);
+    if (index != -1) {
+      return chats[index];
+    }
+
+    final chat = Chat(
+        id: id ?? Uuid().v4(),
+        contactPhoneNumber: contact.phoneNumber,
+        contact: contact,
+        mostRecentMessage: mostRecentMessage,
+        chatType: chatType);
+
+    chatService.insert(chat);
+    return chat;
+  }
+
+  void reload() {
+    contactService.fetchAll().then((contactList) {
+      model.replaceContacts(contactList);
+    });
   }
 }

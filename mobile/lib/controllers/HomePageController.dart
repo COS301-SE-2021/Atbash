f313dd65278dd2ae7c7e2ce3a74f3aa2e2107e14
DYLERN <dylan@squarepeg.co.za>
2021-08-31 14:45:02 +0200

    Added HomePageController

---
+++ mobile/lib/controllers/HomePageController.dart
@@ -0,0 +1,29 @@
+import 'package:get_it/get_it.dart';
+import 'package:mobile/models/HomePageModel.dart';
+import 'package:mobile/services/ChatService.dart';
+import 'package:mobile/services/UserService.dart';
+
+class HomePageController {
+  final UserService userService = GetIt.I.get();
+  final ChatService chatService = GetIt.I.get();
+
+  final HomePageModel model = HomePageModel();
+
+  HomePageController() {
+    userService
+        .getDisplayName()
+        .then((displayName) => model.userDisplayName = displayName);
+
+    userService.getStatus().then((status) => model.userStatus = status);
+
+    userService
+        .getProfileImage()
+        .then((profileImage) => model.userProfileImage = profileImage);
+
+    chatService.fetchAll().then((chats) => model.replaceChats(chats));
+  }
+
+  void deleteChat(String chatId) {
+    model.removeChat(chatId);
+  }
+}

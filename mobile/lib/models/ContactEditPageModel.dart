10d1977d95f3d02fdfca2705dd765b22e6cf19d0
ConnorMayston <cmayston0911@gmail.com>
2021-08-31 11:44:05 +0200

    Created ContactEditPageModel.dart

---
+++ mobile/lib/models/ContactEditPageModel.dart
@@ -0,0 +1,29 @@
+import 'package:mobx/mobx.dart';
+
+part 'ContactEditPageModel.g.dart';
+
+class ContactEditPageModel = _ContactEditPageModel with _$ContactEditPageModel;
+
+abstract class _ContactEditPageModel with Store {
+  @observable
+  String contactName = "";
+
+  @observable
+  String contactNumber = "";
+
+  @observable
+  String contactProfileImage = "";
+
+  @observable
+  DateTime? contactBirthday;
+
+  @action
+  void setContactName(String name) {
+    contactName = name;
+  }
+
+  @action
+  void setContactBirthday(DateTime birthday) {
+    contactBirthday = birthday;
+  }
+}
---
+++ mobile/lib/models/ContactEditPageModel.g.dart
@@ -0,0 +1,108 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ContactEditPageModel.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$ContactEditPageModel on _ContactEditPageModel, Store {
+  final _$contactNameAtom = Atom(name: '_ContactEditPageModel.contactName');
+
+  @override
+  String get contactName {
+    _$contactNameAtom.reportRead();
+    return super.contactName;
+  }
+
+  @override
+  set contactName(String value) {
+    _$contactNameAtom.reportWrite(value, super.contactName, () {
+      super.contactName = value;
+    });
+  }
+
+  final _$contactNumberAtom = Atom(name: '_ContactEditPageModel.contactNumber');
+
+  @override
+  String get contactNumber {
+    _$contactNumberAtom.reportRead();
+    return super.contactNumber;
+  }
+
+  @override
+  set contactNumber(String value) {
+    _$contactNumberAtom.reportWrite(value, super.contactNumber, () {
+      super.contactNumber = value;
+    });
+  }
+
+  final _$contactProfileImageAtom =
+      Atom(name: '_ContactEditPageModel.contactProfileImage');
+
+  @override
+  String get contactProfileImage {
+    _$contactProfileImageAtom.reportRead();
+    return super.contactProfileImage;
+  }
+
+  @override
+  set contactProfileImage(String value) {
+    _$contactProfileImageAtom.reportWrite(value, super.contactProfileImage, () {
+      super.contactProfileImage = value;
+    });
+  }
+
+  final _$contactBirthdayAtom =
+      Atom(name: '_ContactEditPageModel.contactBirthday');
+
+  @override
+  DateTime? get contactBirthday {
+    _$contactBirthdayAtom.reportRead();
+    return super.contactBirthday;
+  }
+
+  @override
+  set contactBirthday(DateTime? value) {
+    _$contactBirthdayAtom.reportWrite(value, super.contactBirthday, () {
+      super.contactBirthday = value;
+    });
+  }
+
+  final _$_ContactEditPageModelActionController =
+      ActionController(name: '_ContactEditPageModel');
+
+  @override
+  void setContactName(String name) {
+    final _$actionInfo = _$_ContactEditPageModelActionController.startAction(
+        name: '_ContactEditPageModel.setContactName');
+    try {
+      return super.setContactName(name);
+    } finally {
+      _$_ContactEditPageModelActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setContactBirthday(DateTime birthday) {
+    final _$actionInfo = _$_ContactEditPageModelActionController.startAction(
+        name: '_ContactEditPageModel.setContactBirthday');
+    try {
+      return super.setContactBirthday(birthday);
+    } finally {
+      _$_ContactEditPageModelActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+contactName: ${contactName},
+contactNumber: ${contactNumber},
+contactProfileImage: ${contactProfileImage},
+contactBirthday: ${contactBirthday}
+    ''';
+  }
+}

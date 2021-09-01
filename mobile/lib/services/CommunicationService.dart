a97ea8ac1fc2d6e99fa80317ae756a3803c026a9
Blackbird0911 <liamm0912@gmail.com>
2021-09-01 12:45:40 +0200

    CommunicationService.dart: Added ability to send liked messages.

--- mobile/lib/services/CommunicationService.dart
+++ mobile/lib/services/CommunicationService.dart
@@ -27,6 +27,8 @@ class CommunicationService {
   List<void Function(String messageId)> _onDeleteListeners = [];
   List<void Function(String messageId)> _onAckListeners = [];
   List<void Function(List<String> messageIds)> _onAckSeenListeners = [];
+  List<void Function(String messageID, bool liked)> _onMessageLikedListeners =
+      [];
 
   set onMessage(void Function(Message message) cb) =>
       _onMessageListeners.add(cb);
@@ -38,18 +40,24 @@ class CommunicationService {
       _onDeleteListeners.add(cb);
 
   void disposeOnDelete(void Function(String messageId) cb) =>
-      _onDeleteListeners.add(cb);
+      _onDeleteListeners.remove(cb);
 
   set onAck(void Function(String messageId) cb) => _onAckListeners.add(cb);
 
   void disposeOnAck(void Function(String messageId) cb) =>
-      _onAckListeners.add(cb);
+      _onAckListeners.remove(cb);
 
   set onAckSeen(void Function(List<String> messageIds) cb) =>
       _onAckSeenListeners.add(cb);
 
   void disposeOnAckSeen(void Function(List<String> messageIds) cb) =>
-      _onAckSeenListeners.add(cb);
+      _onAckSeenListeners.remove(cb);
+
+  void onMessageLiked(void Function(String messageID, bool liked) cb) =>
+      _onMessageLikedListeners.add(cb);
+
+  void disposeOnMessageLiked(void Function(String messageID, bool liked) cb) =>
+      _onMessageLikedListeners.remove(cb);
 
   CommunicationService(this.encryptionService, this.userService,
       this.chatService, this.contactService, this.messageService) {
@@ -201,6 +209,7 @@ class CommunicationService {
         case "like":
           messageService.setMessageLiked(
               decryptedContents["messageID"], decryptedContents["liked"]);
+          _onMessageLikedListeners.forEach((listener) => listener);
           break;
       }
     }

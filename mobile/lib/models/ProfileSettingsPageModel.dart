393bd4c9a692018f67e931fc2c9135ed724f4f4a
DYLERN <dylan@squarepeg.co.za>
2021-09-01 08:32:42 +0200

    Removed unnecessary todo

--- mobile/lib/models/ProfileSettingsPageModel.dart
+++ mobile/lib/models/ProfileSettingsPageModel.dart
@@ -4,10 +4,10 @@ import 'package:mobx/mobx.dart';
 
 part 'ProfileSettingsPageModel.g.dart';
 
-class ProfileSettingsPageModel = _ProfileSettingsPageModel with _$ProfileSettingsPageModel;
+class ProfileSettingsPageModel = _ProfileSettingsPageModel
+    with _$ProfileSettingsPageModel;
 
-abstract class _ProfileSettingsPageModel with Store{
-  //TODO make not observable?
+abstract class _ProfileSettingsPageModel with Store {
   @observable
   String phoneNumber = "";
 

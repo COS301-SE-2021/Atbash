2c24900ac74caee8a6bab6312ecd3b8f9a0b68de
DYLERN <dylan@squarepeg.co.za>
2021-09-01 13:45:50 +0200

    _handlEvent deletes messages when parsed

--- mobile/lib/services/CommunicationService.dart
+++ mobile/lib/services/CommunicationService.dart
@@ -232,6 +232,8 @@ class CommunicationService {
               .forEach((listener) => listener(messageId, liked));
           break;
       }
+
+      await _deleteMessageFromServer(id);
     }
   }
 

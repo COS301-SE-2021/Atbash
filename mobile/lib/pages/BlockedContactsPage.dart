5da78b6bbe4ad3945630df8dc609e4fb131346ce
Blackbird0911 <liamm0912@gmail.com>
2021-09-02 16:00:26 +0200

    BlockedContactsPage: Created BlockedContactsPage outline.

---
+++ mobile/lib/pages/BlockedContactsPage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/controllers/BlockedContactsPageController.dart';
+
+class BlockedContactsPage extends StatefulWidget {
+  const BlockedContactsPage({Key? key}) : super(key: key);
+
+  @override
+  _BlockedContactsPageState createState() => _BlockedContactsPageState();
+}
+
+class _BlockedContactsPageState extends State<BlockedContactsPage> {
+  final BlockedContactsPageController controller;
+
+  _BlockedContactsPageState() : controller = BlockedContactsPageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text("Blocked Contacts"),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return Container();
+    });
+  }
+}

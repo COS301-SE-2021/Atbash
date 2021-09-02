eec76569c996adf6b6085ed4af9a80d34db79dfb
ConnorMayston <cmayston0911@gmail.com>
2021-09-02 18:24:06 +0200

    Added more tests

--- mobile/test/ProfileSettingsPage/ProfileSettingsPage_test.dart
+++ mobile/test/ProfileSettingsPage/ProfileSettingsPage_test.dart
@@ -2,19 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/controllers/ProfileSettingsPageController.dart';
 import 'package:mobile/pages/ProfileSettingsPage.dart';
+import 'package:mobile/widgets/AvatarIcon.dart';
 import 'package:mockito/annotations.dart';
 
 import 'ProfileSettingsPage_test.mocks.dart';
 
-@GenerateMocks([
-  ProfileSettingsPageController
-])
+@GenerateMocks([ProfileSettingsPageController])
 void main() {
   final controller = MockProfileSettingsPageController();
 
+  testWidgets("Page should contain correct widgets",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: ProfileSettingsPage(
+        setup: true,
+        controller: controller,
+      ),
+    ));
+
+    expect(find.byType(IconButton), findsNWidgets(2));
+    expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.byType(AvatarIcon), findsOneWidget);
+    expect(find.byType(TextButton), findsOneWidget);
+    expect(find.byType(MaterialButton), findsOneWidget);
+  });
+
   group("Widget tests for entering the page for the first time.", () {
-    testWidgets("Info text should be visible",
-        (WidgetTester tester) async {
+    testWidgets("Info text should be visible", (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(
         home: ProfileSettingsPage(
           setup: true,
@@ -26,7 +40,7 @@ void main() {
       expect(find.text("Next"), findsOneWidget);
     });
 
-    testWidgets("Button should display 'next'", (WidgetTester tester) async{
+    testWidgets("Button should display 'next'", (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(
         home: ProfileSettingsPage(
           setup: true,
@@ -56,9 +70,7 @@ void main() {
   });
 
   group("Widget tests for entering the page after initial setup", () {
-    testWidgets(
-        "Info text should not be visible",
-        (WidgetTester tester) async {
+    testWidgets("Info text should not be visible", (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(
         home: ProfileSettingsPage(
           setup: false,
@@ -69,17 +81,15 @@ void main() {
       expect(find.byKey(Key('infoText')), findsNothing);
     });
 
-    testWidgets(
-        "Button should display 'Save'",
-            (WidgetTester tester) async {
-          await tester.pumpWidget(MaterialApp(
-            home: ProfileSettingsPage(
-              setup: false,
-              controller: controller,
-            ),
-          ));
+    testWidgets("Button should display 'Save'", (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: ProfileSettingsPage(
+          setup: false,
+          controller: controller,
+        ),
+      ));
 
-          expect(find.widgetWithText(MaterialButton, "Save"), findsOneWidget);
-        });
+      expect(find.widgetWithText(MaterialButton, "Save"), findsOneWidget);
+    });
   });
 }

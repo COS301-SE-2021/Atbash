56cd79e42dc33f1eb26d1a96cd64eb91a2309ba4
DYLERN <dylan@squarepeg.co.za>
2021-09-03 17:45:43 +0200

    Added callbacks for online events

--- mobile/lib/services/MemoryStoreService.dart
+++ mobile/lib/services/MemoryStoreService.dart
@@ -1,17 +1,37 @@
+import 'package:mobile/util/Tuple.dart';
+
 class MemoryStoreService {
   final List<String> _onlineContacts = [];
+  final List<Tuple<String, void Function(bool online)>>
+      _onContactOnlineListeners = [];
+
   final List<String> _typingContacts = [];
 
   void addOnlineContact(String contactPhoneNumber) {
     _onlineContacts.add(contactPhoneNumber);
+    _onContactOnlineListeners
+        .where((element) => element.first == contactPhoneNumber)
+        .forEach((element) => element.second(true));
   }
 
   void removeOnlineContact(String contactPhoneNumber) {
     _onlineContacts.remove(contactPhoneNumber);
+    _onContactOnlineListeners
+        .where((element) => element.first == contactPhoneNumber)
+        .forEach((element) => element.second(false));
   }
 
   bool isContactOnline(String contactPhoneNumber) {
     return _onlineContacts.contains(contactPhoneNumber);
+  }
+
+  void onContactOnline(
+      String contactPhoneNumber, void Function(bool online) cb) {
+    _onContactOnlineListeners.add(Tuple(contactPhoneNumber, cb));
+  }
+
+  void disposeOnContactOnline(void Function(bool online) cb) {
+    _onContactOnlineListeners.removeWhere((element) => element.second == cb);
   }
 
   void addTypingContact(String contactPhoneNumber) {

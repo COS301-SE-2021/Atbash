31756a321aefbc67af0826344c5f73e6172b58b7
DYLERN <dylan@squarepeg.co.za>
2021-09-03 17:23:52 +0200

    Implemented basic MemoryStoreService

--- mobile/lib/services/MemoryStoreService.dart
+++ mobile/lib/services/MemoryStoreService.dart
@@ -1,1 +1,28 @@
-class MemoryStoreService {}
+class MemoryStoreService {
+  final List<String> _onlineContacts = [];
+  final List<String> _typingContacts = [];
+
+  void addOnlineContact(String contactPhoneNumber) {
+    _onlineContacts.add(contactPhoneNumber);
+  }
+
+  void removeOnlineContact(String contactPhoneNumber) {
+    _onlineContacts.remove(contactPhoneNumber);
+  }
+
+  bool isContactOnline(String contactPhoneNumber) {
+    return _onlineContacts.contains(contactPhoneNumber);
+  }
+
+  void addTypingContact(String contactPhoneNumber) {
+    _typingContacts.add(contactPhoneNumber);
+  }
+
+  void removeTypingContact(String contactPhoneNumber) {
+    _typingContacts.remove(contactPhoneNumber);
+  }
+
+  bool isContactTyping(String contactPhoneNumber) {
+    return _typingContacts.contains(contactPhoneNumber);
+  }
+}

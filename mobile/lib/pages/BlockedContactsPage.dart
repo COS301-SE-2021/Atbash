13d1166617a5c9ddb08c9c7cf24d654cab7b2563
Blackbird0911 <liamm0912@gmail.com>
2021-09-04 13:44:57 +0200

    BlockedContactsPage.dart: Implemented UI Design.

--- mobile/lib/pages/BlockedContactsPage.dart
+++ mobile/lib/pages/BlockedContactsPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/controllers/BlockedContactsPageController.dart';
+import 'package:mobile/dialogs/InputDialog.dart';
+
+import '../constants.dart';
 
 class BlockedContactsPage extends StatefulWidget {
   const BlockedContactsPage({Key? key}) : super(key: key);
@@ -24,14 +27,108 @@ class _BlockedContactsPageState extends State<BlockedContactsPage> {
   AppBar _buildAppBar() {
     return AppBar(
       title: Text("Blocked Contacts"),
+      actions: [
+        IconButton(
+          onPressed: () => _addBlockedContact(),
+          icon: Icon(
+            Icons.add,
+          ),
+          splashRadius: 24,
+        )
+      ],
     );
   }
 
   Widget _buildBody() {
-    return ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          return Container();
-        });
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.zero,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Constants.darkGrey.withOpacity(0.5),
+          ),
+          child: Row(
+            children: [
+              Icon(
+                Icons.search,
+                size: 20,
+              ),
+              Expanded(
+                child: TextField(
+                  onChanged: (String input) {},
+                  expands: false,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    hintText: "Search",
+                    contentPadding: EdgeInsets.all(2),
+                    isDense: true,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildContactItem();
+            }),
+      ],
+    );
+  }
+
+  Widget _buildContactItem() {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            padding: EdgeInsets.all(5),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Number or Name",
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                // Expanded(
+                //   flex: 1,
+                //   child: Text(
+                //     "Blocked X days ago",
+                //     textAlign: TextAlign.center,
+                //   ),
+                // ),
+                IconButton(
+                  onPressed: () => _removeBlockedContact(),
+                  icon: Icon(Icons.cancel),
+                  splashRadius: 24,
+                )
+              ],
+            ),
+          ),
+          Divider(
+            height: 2,
+            thickness: 2,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _addBlockedContact() {
+    final newBlockedNumber =
+        showInputDialog(context, "Please enter the number you wish to block.");
+    //TODO: Add a blocked contact.
+  }
+
+  void _removeBlockedContact() {
+    //TODO: Remove blocked contact.
   }
 }

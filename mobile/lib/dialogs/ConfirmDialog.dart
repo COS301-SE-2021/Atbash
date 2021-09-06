7f35a93a6bf0a009deffb7c5909cec3af0f677d0
DYLERN <dylan@squarepeg.co.za>
2021-09-06 19:09:40 +0200

    Added optional positive and negative choices for confirm dialog

--- mobile/lib/dialogs/ConfirmDialog.dart
+++ mobile/lib/dialogs/ConfirmDialog.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
-Future<bool?> showConfirmDialog(BuildContext context, String message) {
+Future<bool?> showConfirmDialog(
+  BuildContext context,
+  String message, {
+  String positive = "ok",
+  String negative = "cancel",
+}) {
   return showDialog<bool>(
     context: context,
     barrierDismissible: false,
-    builder: (context) => _ConfirmDialog(message),
+    builder: (context) => _ConfirmDialog(message, positive, negative),
   );
 }
 
 class _ConfirmDialog extends StatelessWidget {
   final String message;
+  final String positive;
+  final String negative;
 
-  _ConfirmDialog(this.message);
+  _ConfirmDialog(this.message, this.positive, this.negative);
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +29,13 @@ class _ConfirmDialog extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).pop(false);
           },
-          child: Text("CANCEL"),
+          child: Text(negative.toUpperCase()),
         ),
         TextButton(
           onPressed: () {
             Navigator.of(context).pop(true);
           },
-          child: Text("OK"),
+          child: Text(positive.toUpperCase()),
         ),
       ],
     );

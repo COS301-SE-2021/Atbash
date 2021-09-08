d5637264ae22f23af8d4beab2a33e4b4cfc66650
Blackbird0911 <liamm0912@gmail.com>
2021-09-08 15:37:26 +0200

    Fully implemented controller.

--- mobile/lib/controllers/BlockedContactsPageController.dart
+++ mobile/lib/controllers/BlockedContactsPageController.dart
@@ -1,13 +1,39 @@
 import 'package:get_it/get_it.dart';
+import 'package:mobile/domain/BlockedNumber.dart';
 import 'package:mobile/models/BlockedContactsPageModel.dart';
-import 'package:mobile/services/ContactService.dart';
+import 'package:mobile/services/BlockedNumbersService.dart';
 
 class BlockedContactsPageController {
-  final ContactService contactService = GetIt.I.get();
+  final BlockedNumbersService blockedNumbersService = GetIt.I.get();
 
   final BlockedContactsPageModel model = BlockedContactsPageModel();
 
   BlockedContactsPageController() {
-    contactService.fetchAll().then((contacts) => model.contacts = contacts);
+    reload();
+  }
+
+  void reload() {
+    blockedNumbersService.fetchAll().then((numbers) {
+      model.blockedNumbers = numbers;
+    });
+  }
+
+  void addNumber(String number) {
+    final blockedNumber = new BlockedNumber(phoneNumber: number);
+    try {
+      blockedNumbersService.insert(blockedNumber);
+      model.blockedNumbers.add(blockedNumber);
+    } on duplicateBlockedNumberException {
+      //:TODO display error message
+    }
+  }
+
+  void deleteNumber(String number) {
+    final blockedNumber = new BlockedNumber(phoneNumber: number);
+    try {
+      blockedNumbersService.delete(blockedNumber.phoneNumber);
+      model.blockedNumbers.remove(blockedNumber);
+    } on blockedNumberDoesNotExistException {}
+    //TODO: Display error message
   }
 }

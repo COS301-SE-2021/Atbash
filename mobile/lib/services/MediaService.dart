60855418637ddeb6c604e7f97a7277cebe6f87b0
Targo Dove <u15020275@tuks.co.za>
2021-09-11 00:13:05 +0200

    Reimplemented uploadMedia and fetchMedia methods in MediaService

--- mobile/lib/services/MediaService.dart
+++ mobile/lib/services/MediaService.dart
@@ -1,22 +1,26 @@
 import 'dart:convert';
 
-import 'package:encrypt/encrypt.dart';
+// import 'package:encrypt/encrypt.dart';
+import 'package:cryptography/cryptography.dart';
 import 'package:http/http.dart';
 import 'package:mobile/constants.dart';
 import 'package:uuid/uuid.dart';
 
 class MediaService {
   Future<MediaUpload?> uploadMedia(String base64Media) async {
-    final base16Key = SecureRandom(32).base16;
-    final base16IV = SecureRandom(16).base16;
+    final algorithm = AesGcm.with256bits();
+    final secretKey = await algorithm.newSecretKey();
+    final nonce = algorithm.newNonce();
 
-    final key = Key.fromBase16(base16Key);
-    final iv = IV.fromBase16(base16IV);
+    // Encrypt
+    final secretBox = await algorithm.encrypt(
+      base64Decode(base64Media),
+      secretKey: secretKey,
+      nonce: nonce,
+    );
 
-    final encryptor = Encrypter(AES(key));
-
+    final encryptedMediaBase64 = base64Encode(secretBox.concatenation());
     final mediaId = Uuid().v4();
-    final encryptedMedia = encryptor.encrypt(base64Media, iv: iv);
 
     final uri = Uri.parse(Constants.httpUrl + "upload");
     final body = {"mediaId": mediaId, "method": "PUT"};
@@ -24,13 +28,12 @@ class MediaService {
 
     if (response.statusCode == 200) {
       final uploadUrl = Uri.parse(response.body);
-      final uploadResponse = await put(uploadUrl, body: encryptedMedia.base64);
+      final uploadResponse = await put(uploadUrl, body: encryptedMediaBase64);
 
       if (uploadResponse.statusCode == 200) {
         return MediaUpload(
           mediaId: mediaId,
-          base16Key: base16Key,
-          base16IV: base16IV,
+          secretKeyBase64: base64Encode(await secretKey.extractBytes()),
         );
       } else {
         print("${uploadResponse.statusCode} - ${uploadResponse.body}");
@@ -41,9 +44,10 @@ class MediaService {
   }
 
   Future<String?> fetchMedia(
-      String mediaId, String base16Key, String base16IV) async {
-    final key = Key.fromBase16(base16Key);
-    final iv = IV.fromBase16(base16IV);
+      String mediaId, String secretKeyBase64) async {
+
+    final algorithm = AesGcm.with256bits();
+    final secretKey = SecretKey(base64Decode(secretKeyBase64));
 
     final uri = Uri.parse(Constants.httpUrl + "upload");
     final body = {"mediaId": mediaId, "method": "GET"};
@@ -58,11 +62,21 @@ class MediaService {
       if (mediaResponse.statusCode == 200) {
         print("received image");
 
-        final encryptor = Encrypter(AES(key));
+        final secretBox = SecretBox.fromConcatenation(
+          base64Decode(mediaResponse.body),
+          nonceLength: algorithm.nonceLength,
+          macLength: algorithm.macAlgorithm.macLength,
+        );
+        
+        secretBox.checkMac(macAlgorithm: algorithm.macAlgorithm, secretKey: secretKey, aad: <int>[]);
 
-        final decryptedImage = encryptor.decrypt64(mediaResponse.body, iv: iv);
+        // Decrypt
+        final decryptedImage = await algorithm.decrypt(
+          secretBox,
+          secretKey: secretKey,
+        );
 
-        return decryptedImage;
+        return base64Encode(decryptedImage);
       } else {
         print("${response.statusCode} - ${response.body}");
       }
@@ -70,16 +84,79 @@ class MediaService {
       print("${response.statusCode} - ${response.body}");
     }
   }
+
+  // Future<MediaUpload?> uploadMedia(String base64Media) async {
+  //   final base16Key = SecureRandom(32).base16;
+  //   final base16IV = SecureRandom(16).base16;
+  //
+  //   final key = Key.fromBase16(base16Key);
+  //   final iv = IV.fromBase16(base16IV);
+  //
+  //   final encryptor = Encrypter(AES(key));
+  //
+  //   final mediaId = Uuid().v4();
+  //   final encryptedMedia = encryptor.encrypt(base64Media, iv: iv);
+  //
+  //   final uri = Uri.parse(Constants.httpUrl + "upload");
+  //   final body = {"mediaId": mediaId, "method": "PUT"};
+  //   final response = await post(uri, body: jsonEncode(body));
+  //
+  //   if (response.statusCode == 200) {
+  //     final uploadUrl = Uri.parse(response.body);
+  //     final uploadResponse = await put(uploadUrl, body: encryptedMedia.base64);
+  //
+  //     if (uploadResponse.statusCode == 200) {
+  //       return MediaUpload(
+  //         mediaId: mediaId,
+  //         base16Key: base16Key,
+  //         base16IV: base16IV,
+  //       );
+  //     } else {
+  //       print("${uploadResponse.statusCode} - ${uploadResponse.body}");
+  //     }
+  //   } else {
+  //     print("${response.statusCode} - ${response.body}");
+  //   }
+  // }
+  //
+  // Future<String?> fetchMedia(
+  //     String mediaId, String base16Key, String base16IV) async {
+  //   final key = Key.fromBase16(base16Key);
+  //   final iv = IV.fromBase16(base16IV);
+  //
+  //   final uri = Uri.parse(Constants.httpUrl + "upload");
+  //   final body = {"mediaId": mediaId, "method": "GET"};
+  //   final response = await post(uri, body: jsonEncode(body));
+  //
+  //   if (response.statusCode == 200) {
+  //     print("received mediaUrl");
+  //
+  //     final mediaUrl = Uri.parse(response.body);
+  //     final mediaResponse = await get(mediaUrl);
+  //
+  //     if (mediaResponse.statusCode == 200) {
+  //       print("received image");
+  //
+  //       final encryptor = Encrypter(AES(key));
+  //
+  //       final decryptedImage = encryptor.decrypt64(mediaResponse.body, iv: iv);
+  //
+  //       return decryptedImage;
+  //     } else {
+  //       print("${response.statusCode} - ${response.body}");
+  //     }
+  //   } else {
+  //     print("${response.statusCode} - ${response.body}");
+  //   }
+  // }
 }
 
 class MediaUpload {
   final String mediaId;
-  final String base16Key;
-  final String base16IV;
+  final String secretKeyBase64;
 
   MediaUpload({
     required this.mediaId,
-    required this.base16Key,
-    required this.base16IV,
+    required this.secretKeyBase64,
   });
 }

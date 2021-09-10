66c1d5fc9512d04a96453e8ffdbae3af3acf7c06
Targo Dove <u15020275@tuks.co.za>
2021-09-11 01:42:31 +0200

    Removed commented out functions from MediaService

--- mobile/lib/services/MediaService.dart
+++ mobile/lib/services/MediaService.dart
@@ -68,7 +68,7 @@ class MediaService {
           macLength: algorithm.macAlgorithm.macLength,
         );
         
-        secretBox.checkMac(macAlgorithm: algorithm.macAlgorithm, secretKey: secretKey, aad: <int>[]);
+        // secretBox.checkMac(macAlgorithm: algorithm.macAlgorithm, secretKey: secretKey, aad: <int>[]);
 
         // Decrypt
         final decryptedImage = await algorithm.decrypt(
@@ -84,71 +84,6 @@ class MediaService {
       print("${response.statusCode} - ${response.body}");
     }
   }
-
-  // Future<MediaUpload?> uploadMedia(String base64Media) async {
-  //   final base16Key = SecureRandom(32).base16;
-  //   final base16IV = SecureRandom(16).base16;
-  //
-  //   final key = Key.fromBase16(base16Key);
-  //   final iv = IV.fromBase16(base16IV);
-  //
-  //   final encryptor = Encrypter(AES(key));
-  //
-  //   final mediaId = Uuid().v4();
-  //   final encryptedMedia = encryptor.encrypt(base64Media, iv: iv);
-  //
-  //   final uri = Uri.parse(Constants.httpUrl + "upload");
-  //   final body = {"mediaId": mediaId, "method": "PUT"};
-  //   final response = await post(uri, body: jsonEncode(body));
-  //
-  //   if (response.statusCode == 200) {
-  //     final uploadUrl = Uri.parse(response.body);
-  //     final uploadResponse = await put(uploadUrl, body: encryptedMedia.base64);
-  //
-  //     if (uploadResponse.statusCode == 200) {
-  //       return MediaUpload(
-  //         mediaId: mediaId,
-  //         base16Key: base16Key,
-  //         base16IV: base16IV,
-  //       );
-  //     } else {
-  //       print("${uploadResponse.statusCode} - ${uploadResponse.body}");
-  //     }
-  //   } else {
-  //     print("${response.statusCode} - ${response.body}");
-  //   }
-  // }
-  //
-  // Future<String?> fetchMedia(
-  //     String mediaId, String base16Key, String base16IV) async {
-  //   final key = Key.fromBase16(base16Key);
-  //   final iv = IV.fromBase16(base16IV);
-  //
-  //   final uri = Uri.parse(Constants.httpUrl + "upload");
-  //   final body = {"mediaId": mediaId, "method": "GET"};
-  //   final response = await post(uri, body: jsonEncode(body));
-  //
-  //   if (response.statusCode == 200) {
-  //     print("received mediaUrl");
-  //
-  //     final mediaUrl = Uri.parse(response.body);
-  //     final mediaResponse = await get(mediaUrl);
-  //
-  //     if (mediaResponse.statusCode == 200) {
-  //       print("received image");
-  //
-  //       final encryptor = Encrypter(AES(key));
-  //
-  //       final decryptedImage = encryptor.decrypt64(mediaResponse.body, iv: iv);
-  //
-  //       return decryptedImage;
-  //     } else {
-  //       print("${response.statusCode} - ${response.body}");
-  //     }
-  //   } else {
-  //     print("${response.statusCode} - ${response.body}");
-  //   }
-  // }
 }
 
 class MediaUpload {

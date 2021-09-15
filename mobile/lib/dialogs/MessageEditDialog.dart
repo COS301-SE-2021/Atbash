c835cb7af202e9716709cb4f971b46e5e2b90af2
Blackbird0911 <liamm0912@gmail.com>
2021-09-15 15:48:10 +0200

    Created EditMessageDialog.dart.

---
+++ mobile/lib/dialogs/MessageEditDialog.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Future<bool?> showEditMessageDialog(
+    BuildContext context, String originalMessage) {}
+
+class EditMessageDialog extends StatefulWidget {
+  const EditMessageDialog({Key? key}) : super(key: key);
+
+  @override
+  _EditMessageDialogState createState() => _EditMessageDialogState();
+}
+
+class _EditMessageDialogState extends State<EditMessageDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

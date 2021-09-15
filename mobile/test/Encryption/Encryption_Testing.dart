693753964fc454b512530bf4505320ea25613497
Targo Dove <u15020275@tuks.co.za>
2021-09-15 23:23:11 +0200

    Fixed errors in testing setup for testing blinding signatures

--- mobile/test/Encryption/Encryption_Testing.dart
+++ mobile/test/Encryption/Encryption_Testing.dart
@@ -13,22 +13,65 @@ import 'package:mobile/services/DatabaseService.dart';
 import 'package:mobile/services/EncryptionService.dart';
 import 'package:mobile/services/UserService.dart';
 
+import 'package:pointycastle/export.dart' as Pointy;
 
+import 'package:mockito/mockito.dart';
+
+class MockUserService extends Mock implements UserService {
+  MockUserService() {
+    throwOnMissingStub(this);
+  }
+}
+
+class MockDatabaseService extends Mock implements DatabaseService {
+  MockDatabaseService() {
+    throwOnMissingStub(this);
+  }
+}
+
+class MockIdentityKeyStoreService extends Mock implements IdentityKeyStoreService {
+  MockIdentityKeyStoreService() {
+    throwOnMissingStub(this);
+  }
+}
+
+class MockPreKeyStoreService extends Mock implements PreKeyStoreService {
+  MockPreKeyStoreService() {
+    throwOnMissingStub(this);
+  }
+}
+
+class MockSessionStoreService extends Mock implements SessionStoreService {
+  MockSessionStoreService() {
+    throwOnMissingStub(this);
+  }
+}
+
+class MockSignedPreKeyStoreService extends Mock implements SignedPreKeyStoreService {
+  MockSignedPreKeyStoreService() {
+    throwOnMissingStub(this);
+  }
+}
+
+class MockSignalProtocolStoreService extends Mock implements SignalProtocolStoreService {
+  MockSignalProtocolStoreService() {
+    throwOnMissingStub(this);
+  }
+}
+
+// class FakeUserService extends Fake implements UserService {}
+//
+// class FakeDatabaseService extends Fake implements DatabaseService {}
 
 void main() {
-  final userService = UserService();
-  final databaseService = DatabaseService();
+  final userService = MockUserService();
+  final databaseService = MockDatabaseService();
 
-  final identityKeyStoreService = IdentityKeyStoreService(databaseService);
-  final preKeyStoreService = PreKeyStoreService(databaseService);
-  final sessionStoreService = SessionStoreService(databaseService);
-  final signedPreKeyStoreService = SignedPreKeyStoreService(databaseService);
-  final signalProtocolStoreService = SignalProtocolStoreService(
-    preKeyStoreService,
-    sessionStoreService,
-    signedPreKeyStoreService,
-    identityKeyStoreService,
-  );
+  final identityKeyStoreService = MockIdentityKeyStoreService();
+  final preKeyStoreService = MockPreKeyStoreService();
+  final sessionStoreService = MockSessionStoreService();
+  final signedPreKeyStoreService = MockSignedPreKeyStoreService();
+  final signalProtocolStoreService = MockSignalProtocolStoreService();
   final encryptionService = EncryptionService(
     userService,
     signalProtocolStoreService,
@@ -41,6 +84,13 @@ void main() {
   test('Test 1', () async {
     print("Testing BlindSignatures");
 
+    final sGen = Random.secure();
+    Pointy.SecureRandom secureRandom = Pointy.FortunaRandom();
+    secureRandom.seed(Pointy.KeyParameter(
+        Uint8List.fromList(List.generate(32, (_) => sGen.nextInt(255)))));
+
+    Uint8List salt = secureRandom.nextBytes(20);
+
     RSAKeypair rsaKeypair = RSAKeypair.fromRandom(keySize: 4096);
     final pubRsaKey = rsaKeypair.publicKey.asPointyCastle;
     final priRsaKey = rsaKeypair.privateKey.asPointyCastle;
@@ -49,16 +99,16 @@ void main() {
     print("Message to sign: " + messageToSign);
     BigInt blindingFactor = encryptionService.generateBlindingFactor(pubRsaKey);
 
-    Uint8List blindedMessage = encryptionService.createBlindedMessage(messageToSign, pubRsaKey, blindingFactor);
+    Uint8List blindedMessage = encryptionService.createBlindedMessage(messageToSign, pubRsaKey, blindingFactor, salt);
     print("Blinded Message: " + base64Encode(blindedMessage));
 
     Uint8List signedBlindedMessage = encryptionService.serverSignMessage(blindedMessage, priRsaKey);
     print("Signed Blinded Message: " + base64Encode(signedBlindedMessage));
 
-    Uint8List signedMessage = encryptionService.unblindMessage(signedBlindedMessage, pubRsaKey, blindingFactor);
+    Uint8List signedMessage = encryptionService.unblindMessage(signedBlindedMessage, pubRsaKey, blindingFactor, salt);
     print("Signed Message: " + base64Encode(signedMessage));
 
-    final isVerified = encryptionService.serverVerifyMessage(messageToSign, signedMessage, pubRsaKey);
+    final isVerified = encryptionService.serverVerifyMessage(messageToSign, signedMessage, pubRsaKey, salt);
     print("Verified: " + isVerified.toString());
 
     expect(3, 3*2/2);

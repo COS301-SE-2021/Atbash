b988a32db890a44ee75d106fbf0f28de662bcef1
Targo Dove <u15020275@tuks.co.za>
2021-09-16 23:13:39 +0200

    Created MessageboxService class

---
+++ mobile/lib/services/MessageboxService.dart
@@ -0,0 +1,23 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+//Encryption
+import 'package:mobile/encryption/BlindSignatures.dart';
+import 'package:mobile/encryption/MailboxKeyDBRecord.dart';
+
+//RSA Cryptography
+import 'package:crypton/crypton.dart';
+
+
+class MessageboxService {
+  MessageboxService();
+
+  final _storage = FlutterSecureStorage();
+
+
+
+
+}

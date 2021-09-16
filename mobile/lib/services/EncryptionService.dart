3937bef382d8b4348658a06010f20928c8dff3ca
Targo Dove <u15020275@tuks.co.za>
2021-09-16 23:10:38 +0200

    Added additional Mailbox methods to EncryptionService class

--- mobile/lib/services/EncryptionService.dart
+++ mobile/lib/services/EncryptionService.dart
@@ -8,6 +8,8 @@ import 'dart:math';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile/encryption/BlindSignatures.dart';
+import 'package:mobile/encryption/MailboxKeyDBRecord.dart';
 
 import 'package:mobile/exceptions/DecryptionErrorException.dart';
 import 'package:mobile/exceptions/InvalidNumberException.dart';
@@ -503,7 +505,6 @@ class EncryptionService {
     }
   }
 
-
   List<RSAKeypair> generateRSAKeyPairs(int numPairs){
     List<RSAKeypair> keys = [];
 
@@ -514,7 +515,77 @@ class EncryptionService {
     return keys;
   }
 
+  Future<List<MailboxTokenDBRecord>> getMailboxKeys(int numKeys) async {
+    final blindSignatures = BlindSignatures();
+    List<MailboxTokenDBRecord> tokens = [];
 
+    final keyPairs = generateRSAKeyPairs(numKeys);
+    List<Map<String, Object>> blindedPKs = [];
 
+    for(var k in keyPairs){
+      final message = jsonEncode({
+        "n": k.publicKey.asPointyCastle.n.toString(),
+        "e": k.publicKey.asPointyCastle.publicExponent.toString()
+      });
+      blindedPKs.add(blindSignatures.blind(message, k.publicKey));
+    }
+
+    final url = Uri.parse(Constants.httpUrl + "mailbox/createTokens");
+    final phoneNumber = await _userService.getPhoneNumber();
+    final authTokenEncoded = await getDeviceAuthTokenEncoded();
+
+    List<Map<String, Object>> blindedPKArr = [];
+    for (var i = 0; i < blindedPKs.length; i++) {
+      blindedPKArr.add({
+        "keyId": i,
+        "blindedKey": (blindedPKs[i]["blinded"] as BigInt).toString()
+      });
+    }
+
+    var data = {
+      "authorization": "Bearer $authTokenEncoded",
+      "phoneNumber": phoneNumber,
+      "blindedPKs": blindedPKArr,
+    };
+
+    final response = await http.post(url, body: jsonEncode(data));
+
+    if (response.statusCode == 200) {
+      final responseBodyJson = jsonDecode(response.body) as List;
+      final int numMailboxTokens = await getNumMailboxTokens();
+
+      for(var i = 0; i < responseBodyJson.length; i++){
+        final index = responseBodyJson[i]["tokenId"] as int;
+        final signedPK = responseBodyJson[i]["signedPK"] as String;
+
+        tokens.add(MailboxTokenDBRecord(numMailboxTokens + i, keyPairs[index], BigInt.parse(signedPK)));
+      }
+
+      return tokens;
+    } else {
+      //Soft fail
+      print("Server request was unsuccessful.\nResponse code: " +
+          response.statusCode.toString() +
+          ".\nReason: " +
+          response.body);
+      return tokens; // Empty list
+    }
+  }
+
+  /// This function records the number of mailbox tokens that have been created
+  Future<void> setNumMailboxTokens(int index) async {
+    await _storage.write(key: "num_mailbox_tokens", value: index.toString());
+  }
+
+  /// This function retrieves the number of mailbox tokens that have been created
+  Future<int> getNumMailboxTokens() async {
+    final indexStr = await _storage.read(key: "num_mailbox_tokens");
+    if (indexStr == null) {
+      await setNumServerPreKeys(0);
+      return 0;
+    } else {
+      return int.parse(indexStr);
+    }
+  }
 
 }

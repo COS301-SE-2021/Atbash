ee1c55926cbbac3a63019ab6bc3fb2a6298eaffd
Blackbird0911 <liamm0912@gmail.com>
2021-09-16 08:46:36 +0200

    Completed outline

--- mobile/lib/dialogs/MessageEditDialog.dart
+++ mobile/lib/dialogs/MessageEditDialog.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
 Future<bool?> showEditMessageDialog(
-    BuildContext context, String originalMessage) {}
+    BuildContext context, String originalMessage) {
+  return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => EditMessageDialog(originalMessage));
+}
 
 class EditMessageDialog extends StatefulWidget {
-  const EditMessageDialog({Key? key}) : super(key: key);
+  final String originalMessage;
+
+  EditMessageDialog(this.originalMessage);
 
   @override
   _EditMessageDialogState createState() => _EditMessageDialogState();
 }
 
 class _EditMessageDialogState extends State<EditMessageDialog> {
+  final inputController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    inputController.text = widget.originalMessage;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AlertDialog(
+      content: TextField(
+        controller: inputController,
+      ),
+    );
   }
 }

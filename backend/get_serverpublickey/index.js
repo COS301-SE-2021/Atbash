b32565cced3f205e574065c2c0b211fd4c6c5155
DYLERN <dylan@squarepeg.co.za>
2021-09-17 13:52:08 +0200

    Parsed public key

--- backend/get_serverpublickey/index.js
+++ backend/get_serverpublickey/index.js
@@ -8,11 +8,11 @@ exports.handler = async event => {
 
   //Get the public key from the key manager
 
-  const {n, e, d, p, q, dmp1, dmq1, coeff} = JSON.parse(process.env.MESSAGEBOX_KEYPAIR)
+  const {n, e} = JSON.parse(process.env.MESSAGEBOX_KEYPAIR)
 
   let publicKey = {
-    n: "",
-    e: ""
+    n: BigInt(n),
+    e: BigInt(e)
   };
 
   return {statusCode: 200, body: publicKey}

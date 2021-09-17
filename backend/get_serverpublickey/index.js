174d1fe046e2b22b2b4ce7c2f987158f0dbdfc8b
Targo Dove <u15020275@tuks.co.za>
2021-09-17 14:21:20 +0200

    Updated get_serverpublickey method

--- backend/get_serverpublickey/index.js
+++ backend/get_serverpublickey/index.js
@@ -1,12 +1,6 @@
-const {authenticateAuthenticationToken, uploadKeys} = require("./db_access")
-
 exports.handler = async event => {
   //const utf8Encoder = new TextEncoder("utf-8")
   //const utf8Decoder = new TextDecoder("utf-8")
-
-  // const {} = JSON.parse(event.body)
-
-  //Get the public key from the key manager
 
   const {n, e} = JSON.parse(process.env.MESSAGEBOX_KEYPAIR)
 

74da01ac5c13e34db93efd5bd3fda6060605ca51
Targo Dove <u15020275@tuks.co.za>
2021-09-17 14:27:57 +0200

    Updated get_serverpublickey method

--- backend/get_serverpublickey/index.js
+++ backend/get_serverpublickey/index.js
@@ -5,8 +5,8 @@ exports.handler = async event => {
   const {n, e} = JSON.parse(process.env.MESSAGEBOX_KEYPAIR)
 
   let publicKey = {
-    n: BigInt(n),
-    e: BigInt(e)
+    n: n,
+    e: e
   };
 
   return {statusCode: 200, body: publicKey}

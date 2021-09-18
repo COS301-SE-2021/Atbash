49542003e4ca96c3ebae658dfd8f1816c1d5ed9e
DYLERN <dylan@squarepeg.co.za>
2021-09-18 14:30:58 +0200

    Fixed validation error

--- backend/does_number_exist/db_access.js
+++ backend/does_number_exist/db_access.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk")
 
-const db = new AWS.DynamoDB
+const db = new AWS.DynamoDB.DocumentClient({apiVersion: "2012-08-10", region: process.env.AWS_REGION})
 
 exports.doesPhoneNumberExist = async phoneNumber => {
     try {

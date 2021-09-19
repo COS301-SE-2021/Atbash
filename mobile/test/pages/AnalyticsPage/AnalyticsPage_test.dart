098f4cfddeef6ed4495c19495ba85650058fab37
Josh-Reddy <josh.reddy01@gmail.com>
2021-09-19 20:12:35 +0200

    set up for messages for AnalyticsPage testing

--- mobile/test/pages/AnalyticsPage/AnalyticsPage_test.dart
+++ mobile/test/pages/AnalyticsPage/AnalyticsPage_test.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile/domain/Chat.dart';
+import 'package:mobile/domain/Message.dart';
+import 'package:mobile/pages/AnalyticsPage.dart';
 import 'package:mobile/services/ChatService.dart';
 import 'package:mobile/services/ContactService.dart';
 import 'package:mobile/services/MessageService.dart';
@@ -32,15 +35,6 @@ void main() {
                   status: "Hi"),
               chatType: ChatType.general),
           Chat(
-              id: "456",
-              contactPhoneNumber: "0721233413",
-              contact: Contact(
-                  phoneNumber: "0721233413",
-                  displayName: "Liam",
-                  profileImage: "",
-                  status: "New to Atbash"),
-              chatType: ChatType.general),
-          Chat(
               id: "789",
               contactPhoneNumber: "0836006179",
               contact: Contact(
@@ -50,5 +44,43 @@ void main() {
                   status: "Yum"),
               chatType: ChatType.general)
         ]));
+
+    when(messageService.fetchAllByChatId("123"))
+        .thenAnswer((_) => Future.value([
+              Message(
+                  id: "12345",
+                  chatId: "123",
+                  isIncoming: true,
+                  otherPartyPhoneNumber: "0836006179",
+                  contents: "Hi, how are you?",
+                  timestamp: DateTime.now()),
+              Message(
+                  id: "45643",
+                  chatId: "123",
+                  isIncoming: true,
+                  otherPartyPhoneNumber: "0836006179",
+                  contents: "I'm good. What are you doing?",
+                  timestamp: DateTime.now(),
+                  liked: true)
+            ]));
+
+    when(messageService.fetchAllByChatId("789"))
+        .thenAnswer((_) => Future.value([
+              Message(
+                  id: "54353",
+                  chatId: "789",
+                  isIncoming: true,
+                  otherPartyPhoneNumber: "0837772222",
+                  contents: "I am good and you?",
+                  timestamp: DateTime.now(),
+                  liked: true),
+              Message(
+                  id: "89775",
+                  chatId: "789",
+                  isIncoming: true,
+                  otherPartyPhoneNumber: "0837772222",
+                  contents: "TV and you?",
+                  timestamp: DateTime.now())
+            ]));
   });
 }

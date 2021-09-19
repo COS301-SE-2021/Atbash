76098e77743a71d9a342a6e82be59accb38979c5
Josh-Reddy <josh.reddy01@gmail.com>
2021-09-19 21:20:50 +0200

    created WallpaperPage testing

---
+++ mobile/test/pages/WallpaperPage/WallpaperPage_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/services/SettingsService.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([SettingsService])
+void main() {
+  testWidgets("dwdw", (tester) async {});
+}

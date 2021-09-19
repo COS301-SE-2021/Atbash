5a1fb90b1564b6e79b33bc526b4335e9b89db36e
Josh-Reddy <josh.reddy01@gmail.com>
2021-09-19 17:33:24 +0200

    Created set up for AnalyticsPage testing

--- mobile/test/pages/AnalyticsPage/AnalyticsPage_test.dart
+++ mobile/test/pages/AnalyticsPage/AnalyticsPage_test.dart
@@ -1,7 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/domain/Chat.dart';
 import 'package:mobile/services/ChatService.dart';
 import 'package:mobile/services/ContactService.dart';
 import 'package:mobile/services/MessageService.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mobile/domain/Contact.dart';
+
+import 'AnalyticsPage_test.mocks.dart';
 
 @GenerateMocks([ContactService, MessageService, ChatService])
-void main() {}
+void main() {
+  final contactService = MockContactService();
+  final messageService = MockMessageService();
+  final chatService = MockChatService();
+
+  GetIt.I.registerSingleton<ContactService>(contactService);
+  GetIt.I.registerSingleton<MessageService>(messageService);
+  GetIt.I.registerSingleton<ChatService>(chatService);
+
+  setUpAll((){
+    when(chatService.fetchByChatType(any)).thenAnswer((_) => Future.value([
+      Chat(id: "123", contactPhoneNumber: "0837772222", contact: Contact(phoneNumber: "0837772222", displayName: "John", profileImage: "", status: "Hi"), chatType: ChatType.general),
+      Chat(id: "456", contactPhoneNumber: "0721233413", contact: Contact(phoneNumber: "0721233413", displayName: "Liam", profileImage: "", status: "New to Atbash"), chatType: ChatType.general),
+      Chat(id: "789", contactPhoneNumber: "0836006179", contact: Contact(phoneNumber: "0836006179", displayName: "Dylan", profileImage: "", status: "Yum"), chatType: ChatType.general)
+    ]));
+  });
+
+
+}

374d2d23e6d8db7d58861bf8e46fa9d944072813
Josh-Reddy <josh.reddy01@gmail.com>
2021-09-19 21:43:34 +0200

    Testing for the snackbar when a wallpaper is saved

--- mobile/test/pages/WallpaperPage/WallpaperPage_test.dart
+++ mobile/test/pages/WallpaperPage/WallpaperPage_test.dart
@@ -1,9 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobile/pages/WallpaperPage.dart';
 import 'package:mobile/services/SettingsService.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'WallpaperPage_test.mocks.dart';
 
 @GenerateMocks([SettingsService])
 void main() {
-  testWidgets("dwdw", (tester) async {});
+  final settingsService = MockSettingsService();
+  GetIt.I.registerSingleton<SettingsService>(settingsService);
+
+  testWidgets("Snackbar_appears_when_tick_is_clicked",
+      (WidgetTester tester) async {
+    when(settingsService.getWallpaperImage())
+        .thenAnswer((_) => Future.value(null));
+    await tester.pumpWidget(MaterialApp(
+      home: WallpaperPage(),
+    ));
+
+    await tester.pump();
+
+    final tickButton = find.byKey(Key("WallpaperPage_snackbarAppears"));
+    expect(tickButton, findsOneWidget);
+
+    await tester.tap(tickButton);
+    await tester.pump();
+
+    expect(find.byType(SnackBar), findsOneWidget);
+  });
 }

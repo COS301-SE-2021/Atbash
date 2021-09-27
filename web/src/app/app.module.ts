cdb394f9f83ffc1142616874ee57354e3508652b
DYLERN <dylan@squarepeg.co.za>
2021-09-27 12:18:02 +0200

    Added indent_size config for *.ts files

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { ChatComponent } from "./chat/chat.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ChatComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }

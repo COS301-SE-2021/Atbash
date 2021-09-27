21c8295d3cac8f00d8a1fa3b39644ec2615d71f5
ConnorMayston <cmayston0911@gmail.com>
2021-09-27 16:51:00 +0200

    Updated to accept child id

--- mobile/lib/domain/ChildsChats.dart
+++ mobile/lib/domain/ChildsChats.dart
@@ -1,23 +1,23 @@
 class ChildsChats {
   final String id;
-  final String childPhoneNumber;
+  final String childId;
   final String otherPartyNumber;
   String? otherPartyName;
 
   ChildsChats({
     required this.id,
-    required this.childPhoneNumber,
+    required this.childId,
     required this.otherPartyNumber,
   });
 
   static const String TABLE_NAME = "childs_chats";
   static const String COLUMN_ID = "id";
-  static const String COLUMN_CHILD_PHONE_NUMBER = "child_phone_number";
+  static const String COLUMN_CHILD_ID = "child_id";
   static const String COLUMN_OTHER_PARTY_NUMBER = "other_party_number";
   static const String COLUMN_OTHER_PARTY_NAME = "other_party_name";
   static const String CREATE_TABLE = "create table $TABLE_NAME ("
       "$COLUMN_ID text primary key,"
-      "$COLUMN_CHILD_PHONE_NUMBER text not null,"
+      "$COLUMN_CHILD_ID text not null,"
       "$COLUMN_OTHER_PARTY_NUMBER text not null,"
       "$COLUMN_OTHER_PARTY_NAME text,"
       ");";

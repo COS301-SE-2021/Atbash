355cc3cc3647c5eafcf4d077a77b9da375788612
ConnorMayston <cmayston0911@gmail.com>
2021-09-27 21:21:23 +0200

    Added insert function

--- mobile/lib/services/ChildrenService.dart
+++ mobile/lib/services/ChildrenService.dart
@@ -1,3 +1,5 @@
+import 'dart:core';
+
 import 'package:mobile/domain/Child.dart';
 import 'package:mobile/services/DatabaseService.dart';
 
@@ -37,7 +39,24 @@ class ChildrenService {
     }
   }
 
+  Future<Child> insert(Child child) async {
+    final db = await databaseService.database;
+
+    await db.transaction((txn) async {
+      final existingChild = await txn.query(Child.TABLE_NAME,
+          where: "${Child.COLUMN_PHONE_NUMBER} = ?",
+          whereArgs: [child.phoneNumber]);
+      if (existingChild.isNotEmpty) throw DuplicateChildException();
+
+      await txn.insert(Child.TABLE_NAME, child.toMap());
+    });
+
+    return child;
+  }
+
 
 }
 
 class ChildDoesNotExistException implements Exception {}
+
+class DuplicateChildException implements Exception {}

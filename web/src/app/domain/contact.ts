3ec6abddc9d590b9bf8d02f7c5a1b4d824fa2408
DYLERN <dylan@squarepeg.co.za>
2021-09-27 13:15:44 +0200

    Fixed readonly properties

--- web/src/app/domain/contact.ts
+++ web/src/app/domain/contact.ts
@@ -1,8 +1,8 @@
 export class Contact {
     public readonly phoneNumber: string
-    public readonly displayName: string
-    public readonly status: string
-    public readonly profileImage: string
+    public displayName: string
+    public status: string
+    public profileImage: string
     public birthday: Date | null = null
 
     constructor(phoneNumber: string, displayName: string, status: string, profileImage: string, birthday: Date | null) {

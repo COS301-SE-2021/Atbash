0373bb028c883f0722422f5a55f8067d388651cc
DYLERN <dylan@squarepeg.co.za>
2021-09-27 12:20:24 +0200

    Added SidebarComponent

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { ChatComponent } from "./chat/chat.component";
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ChatComponent
+        ChatComponent,
+        SidebarComponent
     ],
     imports: [
         BrowserModule

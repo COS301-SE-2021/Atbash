b0af2942420f48d1b1bb9f785429231e34f080f4
DYLERN <dylan@squarepeg.co.za>
2021-09-27 13:14:57 +0200

    Added properties to Chat class

--- web/src/app/domain/chat.ts
+++ web/src/app/domain/chat.ts
@@ -1,2 +1,23 @@
+import { Contact } from "./contact";
+import { Message } from "./message";
+
 export class Chat {
+    public readonly id: string
+    public readonly contactPhoneNumber: string
+    public contact: Contact | null = null
+    public readonly chatType: ChatType
+    public mostRecentMessage: Message | null = null
+
+    constructor(id: string, contactPhoneNumber: string, contact: Contact | null, chatType: ChatType, mostRecentMessage: Message | null) {
+        this.id = id;
+        this.contactPhoneNumber = contactPhoneNumber;
+        this.contact = contact;
+        this.chatType = chatType;
+        this.mostRecentMessage = mostRecentMessage;
+    }
+}
+
+enum ChatType {
+    general,
+    private
 }

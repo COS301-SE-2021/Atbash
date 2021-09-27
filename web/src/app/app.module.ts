8e35ff01f714b452efb302bf72c4c42f1de040cf
DYLERN <dylan@squarepeg.co.za>
2021-09-27 12:59:47 +0200

    Added ChatListComponent

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from "./app.component";
 import { ChatComponent } from "./chat/chat.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { TitleBarComponent } from "./sidebar/title-bar/title-bar.component";
+import { ChatListComponent } from './sidebar/chat-list/chat-list.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ChatComponent,
         SidebarComponent,
-        TitleBarComponent
+        TitleBarComponent,
+        ChatListComponent
     ],
     imports: [
         BrowserModule

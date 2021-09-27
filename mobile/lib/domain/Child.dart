774d2eb407afe2b33d528e0e53fa35903aaf8622
ConnorMayston <cmayston0911@gmail.com>
2021-09-27 20:49:25 +0200

    Added toMap and fromMap functions

--- mobile/lib/domain/Child.dart
+++ mobile/lib/domain/Child.dart
@@ -35,6 +35,81 @@ class Child {
     required this.blockDeletingMessages,
   });
 
+  Map<String, Object?> toMap() {
+    return {
+      COLUMN_ID: id,
+      COLUMN_PHONE_NUMBER: phoneNumber,
+      COLUMN_NAME: name,
+      COLUMN_PIN: pin,
+      COLUMN_EDITABLE_SETTINGS: editableSettings,
+      COLUMN_BLUR_IMAGES: blurImages,
+      COLUMN_SAFE_MODE: safeMode,
+      COLUMN_SHARE_PROFILE_PICTURE: shareProfilePicture,
+      COLUMN_SHARE_STATUS: shareStatus,
+      COLUMN_SHARE_READ_RECEIPTS: shareReadReceipts,
+      COLUMN_SHARE_BIRTHDAY: shareBirthday,
+      COLUMN_LOCKED_ACCOUNT: lockedAccount,
+      COLUMN_PRIVATE_CHAT_ACCESS: privateChatAccess,
+      COLUMN_BLOCK_SAVE_MEDIA: blockSaveMedia,
+      COLUMN_BLOCK_EDITING_MESSAGES: blockEditingMessages,
+      COLUMN_BLOCK_DELETING_MESSAGES: blockDeletingMessages,
+    };
+  }
+
+  static Child? fromMap(Map<String, Object?> map) {
+    final id = map[COLUMN_ID] as String?;
+    final phoneNumber = map[COLUMN_PHONE_NUMBER] as String?;
+    final name = map[COLUMN_NAME] as String?;
+    final pin = map[COLUMN_PIN] as String?;
+    final editableSettings = map[COLUMN_EDITABLE_SETTINGS] as bool?;
+    final blurImages = map[COLUMN_BLUR_IMAGES] as bool?;
+    final safeMode = map[COLUMN_SAFE_MODE] as bool?;
+    final shareProfilePicture = map[COLUMN_SHARE_PROFILE_PICTURE] as bool?;
+    final shareStatus = map[COLUMN_SHARE_STATUS] as bool?;
+    final shareReadReceipts = map[COLUMN_SHARE_READ_RECEIPTS] as bool?;
+    final shareBirthday = map[COLUMN_SHARE_BIRTHDAY] as bool?;
+    final lockedAccount = map[COLUMN_LOCKED_ACCOUNT] as bool?;
+    final privateChatAccess = map[COLUMN_PRIVATE_CHAT_ACCESS] as bool?;
+    final blockSaveMedia = map[COLUMN_BLOCK_SAVE_MEDIA] as bool?;
+    final blockEditingMessages = map[COLUMN_BLOCK_EDITING_MESSAGES] as bool?;
+    final blockDeletingMessages = map[COLUMN_BLOCK_DELETING_MESSAGES] as bool?;
+
+    if (id != null &&
+        phoneNumber != null &&
+        name != null &&
+        pin != null &&
+        editableSettings != null &&
+        blurImages != null &&
+        safeMode != null &&
+        shareProfilePicture != null &&
+        shareStatus != null &&
+        shareReadReceipts != null &&
+        shareBirthday != null &&
+        lockedAccount != null &&
+        privateChatAccess != null &&
+        blockSaveMedia != null &&
+        blockEditingMessages != null &&
+        blockDeletingMessages != null) {
+      return Child(
+          id: id,
+          phoneNumber: phoneNumber,
+          name: name,
+          pin: pin,
+          editableSettings: editableSettings,
+          blurImages: blurImages,
+          safeMode: safeMode,
+          shareProfilePicture: shareProfilePicture,
+          shareStatus: shareStatus,
+          shareReadReceipts: shareReadReceipts,
+          shareBirthday: shareBirthday,
+          lockedAccount: lockedAccount,
+          privateChatAccess: privateChatAccess,
+          blockSaveMedia: blockSaveMedia,
+          blockEditingMessages: blockEditingMessages,
+          blockDeletingMessages: blockDeletingMessages);
+    }
+  }
+
   static const String TABLE_NAME = "child";
   static const String COLUMN_ID = "id";
   static const String COLUMN_PHONE_NUMBER = "phone_number";

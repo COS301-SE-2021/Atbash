4c1dd6bde7d41c019676a7530377ac47e849754f
DYLERN <dylan@squarepeg.co.za>
2021-09-27 13:13:08 +0200

    Added properties to Contact class

--- web/src/app/domain/contact.ts
+++ web/src/app/domain/contact.ts
@@ -1,2 +1,15 @@
 export class Contact {
+    public readonly phoneNumber: string
+    public readonly displayName: string
+    public readonly status: string
+    public readonly profileImage: string
+    public birthday: Date | null = null
+
+    constructor(phoneNumber: string, displayName: string, status: string, profileImage: string, birthday: Date | null) {
+        this.phoneNumber = phoneNumber;
+        this.displayName = displayName;
+        this.status = status;
+        this.profileImage = profileImage;
+        this.birthday = birthday;
+    }
 }

0f6a842a8257991052e24339c709fefd84689fff
DYLERN <dylan@squarepeg.co.za>
2021-09-27 15:28:22 +0200

    Added ChatTitleBar content with mock values

--- web/src/app/chat/chat-title-bar/chat-title-bar.component.ts
+++ web/src/app/chat/chat-title-bar/chat-title-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-chat-title-bar',
-  templateUrl: './chat-title-bar.component.html',
-  styleUrls: ['./chat-title-bar.component.scss']
+    selector: "app-chat-title-bar",
+    templateUrl: "./chat-title-bar.component.html",
+    styleUrls: ["./chat-title-bar.component.scss"]
 })
 export class ChatTitleBarComponent implements OnInit {
 
-  constructor() { }
+    // LOCAL STATE
+    @Input() profileImage: string | null = null
+    @Input() displayName: string | null = null
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
 
 }

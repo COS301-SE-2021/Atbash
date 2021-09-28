93ca5594be5f700744ca229a4c2a4a10fd62b3fe
ConnorMayston <cmayston0911@gmail.com>
2021-09-28 10:45:54 +0200

    Fixed class to have child phoneNumber instead of ID

--- mobile/lib/domain/ChildChat.dart
+++ mobile/lib/domain/ChildChat.dart
@@ -1,19 +1,19 @@
 class ChildChat {
   final String id;
-  final String childId;
+  final String childPhoneNumber;
   final String otherPartyNumber;
   String? otherPartyName;
 
   ChildChat(
       {required this.id,
-      required this.childId,
+      required this.childPhoneNumber,
       required this.otherPartyNumber,
       this.otherPartyName});
 
   Map<String, Object?> toMap() {
     return {
       COLUMN_ID: id,
-      COLUMN_CHILD_ID: childId,
+      COLUMN_CHILD_PHONE_NUMBER: childPhoneNumber,
       COLUMN_OTHER_PARTY_NUMBER: otherPartyNumber,
       COLUMN_OTHER_PARTY_NAME: otherPartyName,
     };
@@ -21,14 +21,14 @@ class ChildChat {
 
   static ChildChat? fromMap(Map<String, Object?> map) {
     final id = map[COLUMN_ID] as String?;
-    final childId = map[COLUMN_CHILD_ID] as String?;
+    final childPhoneNumber = map[COLUMN_CHILD_PHONE_NUMBER] as String?;
     final otherPartyNumber = map[COLUMN_OTHER_PARTY_NUMBER] as String?;
     final otherPartyName = map[COLUMN_OTHER_PARTY_NAME] as String?;
 
-    if (id != null && childId != null && otherPartyNumber != null) {
+    if (id != null && childPhoneNumber != null && otherPartyNumber != null) {
       return ChildChat(
           id: id,
-          childId: childId,
+          childPhoneNumber: childPhoneNumber,
           otherPartyNumber: otherPartyNumber,
           otherPartyName: otherPartyName);
     }
@@ -36,12 +36,12 @@ class ChildChat {
 
   static const String TABLE_NAME = "child_chat";
   static const String COLUMN_ID = "id";
-  static const String COLUMN_CHILD_ID = "child_id";
+  static const String COLUMN_CHILD_PHONE_NUMBER = "child_phone_number";
   static const String COLUMN_OTHER_PARTY_NUMBER = "other_party_number";
   static const String COLUMN_OTHER_PARTY_NAME = "other_party_name";
   static const String CREATE_TABLE = "create table $TABLE_NAME ("
       "$COLUMN_ID text primary key,"
-      "$COLUMN_CHILD_ID text not null,"
+      "$COLUMN_CHILD_PHONE_NUMBER text not null,"
       "$COLUMN_OTHER_PARTY_NUMBER text not null,"
       "$COLUMN_OTHER_PARTY_NAME text,"
       ");";

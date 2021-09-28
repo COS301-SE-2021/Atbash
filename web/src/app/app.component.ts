309e36c82d01643651a59847880a6c6ddae09912
DYLERN <dylan@squarepeg.co.za>
2021-09-28 07:16:16 +0200

    Show loading page when loading

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { CommunicationService } from "./services/communication.service";
 
 @Component({
     selector: "app-root",
@@ -6,4 +7,12 @@ import { Component } from "@angular/core";
     styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+
+    loaded = false
+
+    constructor(private com: CommunicationService) {
+        setTimeout(() => {
+            this.loaded = true
+        }, 5000)
+    }
 }

f2f1e7ab45585f07a9fe98431163b0dbb818b02a
ConnorMayston <cmayston0911@gmail.com>
2021-09-28 15:20:13 +0200

    Finished functions

--- mobile/lib/services/ChildChatService.dart
+++ mobile/lib/services/ChildChatService.dart
@@ -23,12 +23,30 @@ class ChildChatService {
     return chats;
   }
 
+  Future<ChildChat> fetchByNumbers(
+      String childNumber, String otherNumber) async {
+    final db = await databaseService.database;
+
+    final result = await db.query(ChildChat.TABLE_NAME,
+        where:
+            "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?",
+        whereArgs: [childNumber, otherNumber]);
+
+    if (result.isNotEmpty) {
+      final chat = ChildChat.fromMap(result.first);
+      if (chat == null) throw ChildChatDoesNotExistException();
+
+      return chat;
+    }
+
+    throw ChildChatDoesNotExistException();
+  }
+
   Future<void> insert(ChildChat chat) async {
     final db = await databaseService.database;
 
     await db.transaction((txn) async {
       final chatAlreadyExists = await txn.query(ChildChat.TABLE_NAME,
-          //TODO Ive implemented the dual key here
           where:
               "${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ? AND ${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ?",
           whereArgs: [chat.otherPartyNumber, chat.childPhoneNumber]);
@@ -39,12 +57,15 @@ class ChildChatService {
     });
   }
 
-  Future<void> updateOtherPartyNameByNumbers(String childNumber, String otherNumber, String name) async {
+  Future<void> updateOtherPartyNameByNumbers(
+      String childNumber, String otherNumber, String name) async {
     final db = await databaseService.database;
 
     //Check if exists?
     final response = await db.query(ChildChat.TABLE_NAME,
-        where: "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?", whereArgs: [childNumber, otherNumber]);
+        where:
+            "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?",
+        whereArgs: [childNumber, otherNumber]);
 
     if (response.isNotEmpty) {
       final chat = ChildChat.fromMap(response.first);
@@ -61,13 +82,25 @@ class ChildChatService {
 
     await db.transaction((txn) async {
       final chatExists = await txn.query(ChildChat.TABLE_NAME,
-          where: "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?", whereArgs: [childNumber, otherNumber]);
+          where:
+              "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?",
+          whereArgs: [childNumber, otherNumber]);
 
       if (chatExists.isEmpty) throw ChildChatDoesNotExistException();
 
       await txn.delete(ChildChat.TABLE_NAME,
-          where: "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?", whereArgs: [childNumber, otherNumber]);
+          where:
+              "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ? AND ${ChildChat.COLUMN_OTHER_PARTY_NUMBER} = ?",
+          whereArgs: [childNumber, otherNumber]);
     });
+  }
+
+  Future<void> deleteAllByNumber(String childNumber) async {
+    final db = await databaseService.database;
+
+    await db.delete(ChildChat.TABLE_NAME,
+        where: "${ChildChat.COLUMN_CHILD_PHONE_NUMBER} = ?",
+        whereArgs: [childNumber]);
   }
 }
 

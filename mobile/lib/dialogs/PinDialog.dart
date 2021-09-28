2d5ae59b9d7928c8d05edcff9510a0cc1b1d726f
Blackbird0911 <liamm0912@gmail.com>
2021-09-28 15:03:27 +0200

    Created PinDialog.dart

---
+++ mobile/lib/dialogs/PinDialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PinDialog extends StatefulWidget {
+  const PinDialog({Key? key}) : super(key: key);
+
+  @override
+  _PinDialogState createState() => _PinDialogState();
+}
+
+class _PinDialogState extends State<PinDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

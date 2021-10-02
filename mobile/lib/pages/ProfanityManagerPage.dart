5274c945ec55d6d7a97083cbb2f1ffe56dc51213
Blackbird0911 <liamm0912@gmail.com>
2021-10-02 11:54:20 +0200

    ProfanityManagerPage.dart: Created UI.

--- mobile/lib/pages/ProfanityManagerPage.dart
+++ mobile/lib/pages/ProfanityManagerPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/constants.dart';
+import 'package:mobile/dialogs/ConfirmDialog.dart';
+import 'package:mobile/pages/ProfanityPackageManagerPage.dart';
 
 class ProfanityManagerPage extends StatefulWidget {
   const ProfanityManagerPage({Key? key}) : super(key: key);
@@ -9,32 +11,21 @@ class ProfanityManagerPage extends StatefulWidget {
 }
 
 class _ProfanityManagerPageState extends State<ProfanityManagerPage> {
+  String choice = "Package 1";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Profanity Management",
-        ),
+        title: Text("Profanity Management"),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 1 / 6),
-            decoration: BoxDecoration(
-                // border: Border(
-                //   bottom: BorderSide(
-                //     color: Colors.black,
-                //   ),
-                // ),
-                ),
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(
-                  color: Colors.black,
+                border: Border(
+                  bottom: BorderSide(color: Colors.black),
                 ),
               ),
               child: Row(
@@ -42,134 +33,68 @@ class _ProfanityManagerPageState extends State<ProfanityManagerPage> {
                   Icon(Icons.search),
                   Expanded(
                     child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        isDense: true,
-                      ),
+                      decoration: InputDecoration(border: InputBorder.none),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Text(
-                          "Profanity Packages",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(color: Constants.orange),
-                        child: ListTile(
-                          title: Text("Add Profanity Package"),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.add),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                            itemCount: 12,
-                            itemBuilder: (context, index) {
-                              return Center(
-                                child: Container(
-                                  margin: EdgeInsets.symmetric(
-                                      vertical: 10, horizontal: 25),
-                                  padding: EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(24),
-                                    border: Border.all(color: Colors.black),
-                                  ),
-                                  child: InkWell(
-                                    onLongPress: () {},
-                                    child: Text(
-                                      "Package With very long name",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(fontSize: 18),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            }),
-                      ),
-                    ],
-                  ),
-                ),
-                VerticalDivider(
-                  width: 1,
-                  thickness: 1,
-                  color: Colors.black,
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Text(
-                          "Profanity Words",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(color: Constants.orange),
-                        child: ListTile(
-                          title: Text("Add Profanity Word"),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.add),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                            itemCount: 16,
-                            itemBuilder: (context, index) {
-                              return Column(
-                                children: [
-                                  ListTile(
-                                    title: Text("Profanity word!"),
-                                    trailing: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.cancel),
-                                    ),
-                                  ),
-                                  Divider(
-                                    height: 1,
-                                    thickness: 1,
-                                    color: Colors.black,
-                                  )
-                                ],
-                              );
-                            }),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProfanityPackageManagerPage()));
+              },
+              tileColor: Constants.orange,
+              title: Text("Add New Packages"),
+              trailing: Icon(Icons.add),
             ),
-          )
-        ],
+            Expanded(
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (_, i) {
+                  return InkWell(
+                    onLongPress: () {
+                      showConfirmDialog(context,
+                          "Are you sure you want to delete this profanity package?");
+                    },
+                    child: ExpansionTile(
+                      childrenPadding: EdgeInsets.zero,
+                      title: Text("Package Name"),
+                      children: getChildren(5),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  List<Widget> getChildren(int count) {
+    List<Widget> children = [];
+    children.add(
+      ListTile(
+        onTap: () {},
+        title: Text("Add new word to this package."),
+        trailing: Icon(Icons.add),
+      ),
+    );
+    for (int i = 0; i < count; i++) {
+      children.add(
+        ListTile(
+          title: Text("Profanity Word"),
+          dense: true,
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.cancel),
+          ),
+        ),
+      );
+    }
+    return children;
   }
 }

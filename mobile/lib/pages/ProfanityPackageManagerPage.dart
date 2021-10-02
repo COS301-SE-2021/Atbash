25fb68dce251c23c13929a6fcc3e37708fad10a9
Blackbird0911 <liamm0912@gmail.com>
2021-10-02 11:14:22 +0200

    Created ProfanityPackageManagerPage.dart

---
+++ mobile/lib/pages/ProfanityPackageManagerPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfanityPackageManagerPage extends StatefulWidget {
+  const ProfanityPackageManagerPage({Key? key}) : super(key: key);
+
+  @override
+  _ProfanityPackageManagerPageState createState() =>
+      _ProfanityPackageManagerPageState();
+}
+
+class _ProfanityPackageManagerPageState
+    extends State<ProfanityPackageManagerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

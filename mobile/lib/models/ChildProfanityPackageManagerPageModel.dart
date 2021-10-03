c1f98d7be5e63e869aa01a2de5bf70e358111ca2
Blackbird0911 <liamm0912@gmail.com>
2021-10-03 20:16:30 +0200

    Created ChildProfanityPackageManagerPageModel.dart

---
+++ mobile/lib/models/ChildProfanityPackageManagerPageModel.dart
@@ -0,0 +1,18 @@
+import 'package:mobile/domain/StoredProfanityWord.dart';
+import 'package:mobile/util/Tuple.dart';
+import 'package:mobx/mobx.dart';
+
+part 'ChildProfanityPackageManagerPageModel.g.dart';
+
+class ChildProfanityPackageManagerPageModel = _ChildProfanityPackageManagerPageModel
+    with _$ChildProfanityPackageManagerPageModel;
+
+abstract class _ChildProfanityPackageManagerPageModel with Store {
+  @observable
+  ObservableList<StoredProfanityWord> storedProfanityWords =
+      <StoredProfanityWord>[].asObservable();
+
+  @observable
+  ObservableList<Tuple<int, String>> packageCounts =
+      <Tuple<int, String>>[].asObservable();
+}

89de855217c601c16fc8e8e93f7436e1de7b3d4c
Josh-Reddy <josh.reddy01@gmail.com>
2021-10-04 12:27:04 +0200

    updated RegexGeneration

--- mobile/lib/util/RegexGeneration.dart
+++ mobile/lib/util/RegexGeneration.dart
@@ -1,15 +1,18 @@
 String generateRegex(String word) {
   String newWord = word.replaceAll(RegExp(r'e'), '[e3]');
-  newWord = newWord.replaceAll(RegExp(r's'), '[s\$]');
-  newWord = newWord.replaceAll(RegExp(r'a'), '[a@]');
-  newWord = newWord.replaceAll(RegExp(r'l'), '[l1]');
-  newWord = newWord.replaceAll(RegExp(r'i'), '[i!]');
+  newWord = newWord.replaceAll(RegExp(r's'), '[s5z2\$]');
+  newWord = newWord.replaceAll(RegExp(r'z'), '[z2s5\$]');
+  newWord = newWord.replaceAll(RegExp(r'a'), '[a@4]');
+  newWord = newWord.replaceAll(RegExp(r'l'), '[l1I!|]');
+  newWord = newWord.replaceAll(RegExp(r'i'), '[i!1]');
   newWord = newWord.replaceAll(RegExp(r'o'), '[o0]');
-  newWord = newWord.replaceAll(RegExp(r't'), '[t+]');
+  newWord = newWord.replaceAll(RegExp(r't'), '[t7+]');
   newWord = newWord.replaceAll(RegExp(r'f'), '(ph|f)');
   newWord = newWord.replaceAll(RegExp(r'ph'), '(ph|f)');
   newWord = newWord.replaceAll(RegExp(r'a'), '(a|er)');
   newWord = newWord.replaceAll(RegExp(r'er'), '(a|er)');
+  newWord = newWord.replaceAll(RegExp(r'b'), '[b8]');
+  newWord = newWord.replaceAll(RegExp(r'g'), '[g9]');
 
   return newWord;
 }
